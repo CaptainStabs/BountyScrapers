18f7b6345779eb7138e0938a5f3f52775151e6b5
CaptainStabs <comportfailure@gmail.com>
2021-07-27 13:01:08 -0400

    scra[er

---
+++ google_scraper.py
@@ -0,0 +1,22 @@
+import googlesearch as google
+from toasttab_function import toast_tab_scraper
+
+search_query = "site:https://www.toasttab.com/"
+ignored_domains = ["google"]
+remove_words = ["?mode=create"]
+print("   [*] Loop start")
+with open("urls.txt", "a") as output:
+    for results in google.search(search_query, tld="com", lang="en", num=10, start=0, stop=None, pause=2.0):
+        # print("All result: " + results)
+        if not any(ignored_domain in results for ignored_domain in ignored_domains):
+            # print("   All result: " + results)
+            if any(remove_word in results for remove_word in remove_words):
+                results = results.replace(remove_word, "")
+                output.write(results)
+                print(results)
+                try:
+                    print("Scraping")
+                    toast_tab_scraper(results)
+                except exception:
+                    print(exception)
+                    pass
