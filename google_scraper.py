4cc51cfbb67b601417db376a02661a883467697e
CaptainStabs <comportfailure@gmail.com>
2021-07-28 11:46:11 -0400

    removed unneeded open statement

--- google_scraper.py
+++ google_scraper.py
@@ -18,14 +18,14 @@ print("   [*] Loop start")
 #                 print(exception)
 #                 pass
 
-with open("urls.txt", "a") as output:
-    for results in google.search(search_query, tld="com", lang="en", num=10, start=0, stop=None, pause=2.0):
-        # print("All result: " + results
-        # print("   All result: " + results)
-        print(results)
-        try:
-            print("Scraping")
-            toast_tab_scraper(results)
-        except:
-            # print(exception)
-            pass
+
+for results in google.search(search_query, tld="com", lang="en", num=10, start=0, stop=None, pause=0.3):
+    # print("All result: " + results
+    # print("   All result: " + results)
+    print(results)
+    try:
+        print("Scraping")
+        toast_tab_scraper(results, skip_existing=True, pull_master=False)
+    except:
+        # print(exception)
+        pass
