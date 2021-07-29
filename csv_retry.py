fc41e051b87d981f49dca6fb719ea71f1e3ee77b
CaptainStabs <comportfailure@gmail.com>
2021-07-29 14:39:51 -0400

    This all didn't need to be in the for loop

--- csv_retry.py
+++ csv_retry.py
@@ -86,73 +86,73 @@ def check_if_exists():
                         branch_name1 = "add_" + row["restaurant_name"].replace(" ", "").lower()
                         branch_name2 = "add_" + row["restaurant_name"].replace(" ", "-").lower()
 
-                        print("      [*] Finding branch's name...")
+                    print("      [*] Finding branch's name...")
+                    try:
+                        print("         [*] Checking out branch: " + branch_name1)
+                        db.checkout(branch=branch_name1)
+                        branch_name = branch_name1
+                    except:
+                        print("         [!] That didn't work, trying with: " + branch_name2)
+                        pass
                         try:
-                            print("         [*] Checking out branch: " + branch_name1)
-                            db.checkout(branch=branch_name1)
-                            branch_name = branch_name1
+                            db.checkout(branch=branch_name2)
+                            branch_name = branch_name2
                         except:
-                            print("         [!] That didn't work, trying with: " + branch_name2)
+                            branch_name3 = input("         [!] That also didn't work. Type the branchname: ")
+                            db.checkout(branch=branch_name3)
+                            branch_name = branch_name3
                             pass
-                            try:
-                                db.checkout(branch=branch_name2)
-                                branch_name = branch_name2
-                            except:
-                                branch_name3 = input("         [!] That also didn't work. Type the branchname: ")
-                                db.checkout(branch=branch_name3)
-                                branch_name = branch_name3
-                                pass
-                        print("         [*] That worked! branchname: " + branch_name)
-                        # response = requests.request("POST", url, headers=headers, data=payload)  # Don't need this anymore, used to check if the branch existed in  a PR
-                        print("      [*] Checking if branch has a PR...")
-                        if branch_name not in list_branches:
-                            print("         [*] PR does not exist, attempting to write...")
-                            try:
-                                print("            [*] Trying to write to db...")
-                                dolt.write_file(dolt=db, table="menu_items", file_handle=open(root + file, "r"), import_mode="create", commit=True, commit_message="Add data", do_continue=True)
-                                    # dolt.write_file(dolt=db, table="menu_items", file_handle=open(filename, "r"), import_mode="create", do_continue=True)
-                                print("            [*] Trying to push to remote")
-                                db.push(remote="origin", set_upstream=True, refspec=branch_name)
+                    print("         [*] That worked! branchname: " + branch_name)
+                    # response = requests.request("POST", url, headers=headers, data=payload)  # Don't need this anymore, used to check if the branch existed in  a PR
+                    print("      [*] Checking if branch has a PR...")
+                    if branch_name not in list_branches:
+                        print("         [*] PR does not exist, attempting to write...")
+                        try:
+                            print("            [*] Trying to write to db...")
+                            dolt.write_file(dolt=db, table="menu_items", file_handle=open(root + file, "r"), import_mode="create", commit=True, commit_message="Add data", do_continue=True)
+                                # dolt.write_file(dolt=db, table="menu_items", file_handle=open(filename, "r"), import_mode="create", do_continue=True)
+                            print("            [*] Trying to push to remote")
+                            db.push(remote="origin", set_upstream=True, refspec=branch_name)
 
-                                print("            [*] Trying to create PR...")
-                                pr_name = branch_name.replace("_"," ").replace("-", " ") + " " + identifier
-                                # dolt_url = "https://www.dolthub.com/graphql"
-                                payload = json.dumps({
-                                    "operationName": "CreatePullRequestWithForks",
-                                    "variables": {
-                                        "title": f"{pr_name}",
-                                        "description": "",
-                                        "fromBranchName": f"{branch_name}",
-                                        "toBranchName": "master",
-                                        "fromBranchOwnerName": "captainstabs",
-                                        "fromBranchRepoName": "menus",
-                                        "toBranchOwnerName": "dolthub",
-                                        "toBranchRepoName": "menus",
-                                        "parentOwnerName": "dolthub",
-                                        "parentRepoName": "menus"
-                                    },
-                                    "query": "mutation CreatePullRequestWithForks($title: String!, $description: String!, $fromBranchName: String!, $toBranchName: String!, $fromBranchRepoName: String!, $fromBranchOwnerName: String!, $toBranchRepoName: String!, $toBranchOwnerName: String!, $parentRepoName: String!, $parentOwnerName: String!) {\n  createPullWithForks(\n    title: $title\n    description: $description\n    fromBranchName: $fromBranchName\n    toBranchName: $toBranchName\n    fromBranchOwnerName: $fromBranchOwnerName\n    fromBranchRepoName: $fromBranchRepoName\n    toBranchOwnerName: $toBranchOwnerName\n    toBranchRepoName: $toBranchRepoName\n    parentRepoName: $parentRepoName\n    parentOwnerName: $parentOwnerName\n  ) {\n    _id\n    pullId\n    __typename\n  }\n}\n"
-                                })
+                            print("            [*] Trying to create PR...")
+                            pr_name = branch_name.replace("_"," ").replace("-", " ") + " " + identifier
+                            # dolt_url = "https://www.dolthub.com/graphql"
+                            payload = json.dumps({
+                                "operationName": "CreatePullRequestWithForks",
+                                "variables": {
+                                    "title": f"{pr_name}",
+                                    "description": "",
+                                    "fromBranchName": f"{branch_name}",
+                                    "toBranchName": "master",
+                                    "fromBranchOwnerName": "captainstabs",
+                                    "fromBranchRepoName": "menus",
+                                    "toBranchOwnerName": "dolthub",
+                                    "toBranchRepoName": "menus",
+                                    "parentOwnerName": "dolthub",
+                                    "parentRepoName": "menus"
+                                },
+                                "query": "mutation CreatePullRequestWithForks($title: String!, $description: String!, $fromBranchName: String!, $toBranchName: String!, $fromBranchRepoName: String!, $fromBranchOwnerName: String!, $toBranchRepoName: String!, $toBranchOwnerName: String!, $parentRepoName: String!, $parentOwnerName: String!) {\n  createPullWithForks(\n    title: $title\n    description: $description\n    fromBranchName: $fromBranchName\n    toBranchName: $toBranchName\n    fromBranchOwnerName: $fromBranchOwnerName\n    fromBranchRepoName: $fromBranchRepoName\n    toBranchOwnerName: $toBranchOwnerName\n    toBranchRepoName: $toBranchRepoName\n    parentRepoName: $parentRepoName\n    parentOwnerName: $parentOwnerName\n  ) {\n    _id\n    pullId\n    __typename\n  }\n}\n"
+                            })
 
-                                print("   [!] Opening PR")
-                                response = requests.request("POST", dolt_url, headers=dolt_headers, data=payload)
-                                if response.status_code == 200:
-                                    print("      [*] Success!")
-                                else:
-                                    print("      [!] Couldn't open a PR")
-                                    with open("csv_fails.txt", "a") as output:
-                                        output.write(file + ", " + branch_name + ", PR failure" + "\n")
-                                print("      [*] Response: " + response)
-                            except:
-                                print("      [!] Something went wrong!")
+                            print("   [!] Opening PR")
+                            response = requests.request("POST", dolt_url, headers=dolt_headers, data=payload)
+                            if response.status_code == 200:
+                                print("      [*] Success!")
+                            else:
+                                print("      [!] Couldn't open a PR")
                                 with open("csv_fails.txt", "a") as output:
-                                    output.write(file + ", " + branch_name + ", other failure" + "\n")
+                                    output.write(file + ", " + branch_name + ", PR failure" + "\n")
+                            print("      [*] Response: " + response)
+                        except:
+                            print("      [!] Something went wrong!")
+                            with open("csv_fails.txt", "a") as output:
+                                output.write(file + ", " + branch_name + ", other failure" + "\n")
 
 
 
-                #
-                # print(read_csv)
-                # input()
+            #
+            # print(read_csv)
+            # input()
 
 # get_open_prs(payload, headers, url)
 check_if_exists()
