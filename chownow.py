f4f71e5a97ccd89e6064d8d86c37ef844440fa4c
CaptainStabs <comportfailure@gmail.com>
2021-07-31 17:40:46 -0400

    add comments

--- chownow.py
+++ chownow.py
@@ -5,11 +5,13 @@ import doltcli as dolt
 import os
 import time
 import re
+from _cookie import dolt_cookie
 
 
 # db = dolt.Dolt("menus")
 # db.checkout(branch="master")
 
+"""ChowNow request headers"""
 query_headers = {
     'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
     'Accept': 'application/json version=3.0;',
@@ -34,16 +36,22 @@ dolt_headers = {
     'Sec-Fetch-Site': 'same-origin',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Dest': 'empty',
-    'Cookie': '__hssc=234007937.7.1627339712817; __hssrc=1; __hstc=234007937.8c2dbc8618061048c739d393b08463d5.1622138431678.1627327973246.1627339712817.97; _ga=GA1.2.1181270506.1617050482; _gat_gtag_UA_130584753_2=1; _gid=GA1.2.2133862710.1627078097; amplitude_id_fef1e872c952688acd962d30aa545b9edolthub.com=eyJkZXZpY2VJZCI6IjAzMjc5ZDFkLWY4MzQtNDhjNS04MTMyLWQ1NTI2MzUwMDZiNlIiLCJ1c2VySWQiOm51bGwsIm9wdE91dCI6ZmFsc2UsInNlc3Npb25JZCI6MTYyNzMyNDU5ODc1NSwibGFzdEV2ZW50VGltZSI6MTYyNzMyNDY0NDY3MiwiZXZlbnRJZCI6NCwiaWRlbnRpZnlJZCI6MSwic2VxdWVuY2VOdW1iZXIiOjV9; hubspotutk=8c2dbc8618061048c739d393b08463d5; G_ENABLED_IDPS=google; __stripe_mid=4b66cabc-51a2-47a3-8c23-b35a4fcb222f48f55d; __stripe_sid=03c3c926-44c6-4f20-b32f-5b450897d93de0727d; dolthubToken=ldst.v1.6h4rmj56ot1sfhcf3htm9n8im3hb2gfckshkh1aiug0kn45t8j60'
+    'Cookie': f'{dolt_cookie}'
 }
 
 columns = ["name", "restaurant_name", "identifier", "calories", "price_usd"]
 
+"""Open file
+Due to the way that chownow's API requests cities (by coordinates),
+we need to be able to loop through cities
+"""
 print("   [*] Opening list of cities...")
 with open("us_cities.csv", "r") as location_file:
     location_list = csv.reader(location_file)
     next(location_list)  # Skip header
+
     first = True
+
     for row in location_list:
         last = False
         city = row[3]
@@ -52,64 +60,85 @@ with open("us_cities.csv", "r") as location_file:
         lon = row[6]
         print(f"      [*] Current City, State: {city}, {state_code}")
         print(lat, lon)
-        if first:
+
+        if first:  # This needs to be differentiated as the first as we don't have a next page yet
             first_query_url = f"https://api.chownow.com/api/restaurant/?u_lat={lat}&u_lon={lon}"
             first_query_response = requests.request("GET", first_query_url, headers=query_headers)
+
             try:
                 parsed_response = json.loads(first_query_response.text)
+
             except json.decoder.JSONDecodeError as e:
                 print("      [!] Error: " + str(e))
                 print(first_query_response.text)
 
-        else:
+        else:  # (Not first)
             next_page_url = "https://api.chownow.com/" + parsed_response["next"]
             next_page_response = requests.request("GET", next_page_url, headers=query_headers)
             parsed_response = json.loads(next_page_response.text)
 
+
+        """Sleep times are due to the script being too fast
+        when it doesn't handle the db checkout/write/push stuff. (Got rate limitted by cloudflare)
+        """
         if parsed_response["total"] == 0:
             print("      [*] No results, skipping")
             time.sleep(0.9)
             # print(first_query_response.text)
-            continue
+            continue  # Get the next row from location_list
+
         else:
             restaurant_list = parsed_response["restaurants"]
+
             if not parsed_response["next"]:
                 print("      [*] End of City results...")
-                last = True
+                last = True  # Forces the loop to progress at the end
+
             for restaurant_dict in restaurant_list:
-                nutrition_facts = {}
+                nutrition_facts = {}  # Prepare output dict
+
+                ### Access the information from the response
                 # restaurant_dict = restaurant_list[lists]  # Restaurants is a list of dicts
                 restaurant_city = restaurant_dict["address"]["city"]  # This should match the city belonging to the coords, but just to be sure
                 restaurant_state = restaurant_dict["address"]["state"]
                 company_id = restaurant_dict["restaurant_company_id"]
                 restaurant_name = restaurant_dict["short_name"]
                 location_id = restaurant_dict["id"]
-                identifier = restaurant_city + ", " + restaurant_state
+
+                identifier = restaurant_city + ", " + restaurant_state  # For PK in db
                 print(f"      [?] City, state: {identifier}, Company_id: {company_id}, location_id: {location_id}, restaurant_name: {restaurant_name}")
+
                 menu_query = f"https://api.chownow.com/api/restaurant/{company_id}/menu/"
                 menu_response = requests.request("GET", menu_query, headers=query_headers)
                 time.sleep(0.9)
+
                 try:
                     menu_parsed = json.loads(menu_response.text)
+
                 except json.decoder.JSONDecodeError as e:
                     print("      [!] Error: " + str(e))
                     print(first_query_response.text)
-                try:
+
+                try:  # Should probably be protected by a variable defined by a succusful parsed but whatever
                     menu_categories = menu_parsed["menu_categories"]
+
                 except KeyError:  # For whatever reason, the api uses company_id sometimes, and just id for others
                     menu_query = f"https://api.chownow.com/api/restaurant/{location_id}/menu/"
                     menu_response = requests.request("GET", menu_query, headers=query_headers)
                     menu_parsed = json.loads(menu_response.text)
                     time.sleep(0.8)
                     pass
-                    try:
+
+                    try:  # Try accessing menu_categories again
                         menu_categories = menu_parsed["menu_categories"]
-                    except KeyError:
+
+                    except KeyError:  # Other times it requires a time stamp to be appended. format is YYMMDDHHMM
                         menu_query = f"https://api.chownow.com/api/restaurant/{location_id}/menu/" + time.strftime('%Y%m%d') + str(1700)
                         menu_response = requests.request("GET", menu_query, headers=query_headers)
                         menu_parsed = json.loads(menu_response.text)
                         pass
-                        try:
+
+                        try:  # Sometimes the timestamp is different than usual, so I just skip those
                             menu_categories = menu_parsed["menu_categories"]
                         except KeyError:
                             print("      [!] I give up, skipping")
@@ -125,8 +154,9 @@ with open("us_cities.csv", "r") as location_file:
                     # with open("first_query.txt", "a") as f:
                     #     f.write(json.dumps(parsed_response, indent=4))
                     #     input()
+
                 # filename = "add_" + restaurant_name.replace(" ", "_").replace("'","").replace("\n","").replace("&","and").replace("--","-").replace("_|_", "").replace(".", "-").replace("?","").lower() + ".csv"
-                branch_name = "add_" + re.sub("[^0-9a-zA-Z]+", "-", restaurant_name).lower()
+                branch_name = "add_" + re.sub("[^0-9a-zA-Z]+", "-", restaurant_name).lower()  # Strip out nono-alphanumeric characters
                 filename = branch_name + ".csv"
                 # print(json.dumps(menu_parsed, indent=4))
 
@@ -140,19 +170,27 @@ with open("us_cities.csv", "r") as location_file:
                 #     print("        " + str(error))
                 #     db.checkout(branch=branch_name)
                 # print("      [*] Opening file")
+
+                # Write results to file
                 file_path = "./submited/" + filename.replace("/", "-").replace("_|_", "")
                 with open(file_path, "a") as output:
                     writer = csv.DictWriter(output, fieldnames=columns)
+
+                    # Check if the file exists, or if the file is empty
+                    # If it's empty, we need to add the header
                     if not os.path.isfile(file_path) or os.stat(file_path).st_size == 0:
                         writer.writeheader()
                         file_is_new = True
+
                     else:
                         file_is_new = False
+
                     if file_is_new:
                         print("      [*] File is new")
                         for category_dict in menu_categories:
                             category_items = category_dict["items"]
                             # print(category_items)
+
                             for items in category_items:
                                 failed = False
                                 # print(json.dumps(items, indent=4))
@@ -160,26 +198,33 @@ with open("us_cities.csv", "r") as location_file:
                                     try:
                                         print("      [*] There is a size! Size: " + str(items["size"]).upper())
                                         nutrition_facts["name"] = items["name"].replace('\\"', " inch ").replace('"', " inch ").upper() + " - " + str(items["size"]).upper()
-                                    except KeyError as error:
+
+                                    except KeyError as error:  # Means that the API is dumb and inconsisitent
                                         print("      [!] ERROR with size: " + str(error))
                                         nutrition_facts["name"] = items["name"].replace('\\"', " inch ").replace('"', " inch ").upper()
                                         pass
+
                                     nutrition_facts["restaurant_name"] = restaurant_name.upper()
                                     nutrition_facts["identifier"] = "CHOWNOW, " + identifier.upper()
+
                                     try:
                                         nutrition_facts["price_usd"] =  "{:.2f}".format(items["price"])
                                     except KeyError as e:
-                                        failed = True
+                                        failed = True  # I don't want to write a bad row
                                         # print("      [!] " + str(e))
                                         print("      [!] Couldn't find a price")
                                         pass
-                                    if not failed:
+
+                                    if not failed:  # Tries to prevent writing bad rows
                                         writer.writerow(nutrition_facts)
-                                except KeyError as e1:
+
+                                except KeyError as e1:  # Related to first try in this chunk, doesn't happen afaik
                                         failed = True
                                         print("      [!] " + str(e1))
                                         print("      [!] One of the other three columns failed")
                                         pass
+
+                                ### Dolt stuff
                                 # try:
                                 #     print("      [*] Trying to write to db")
                                 #     dolt.write_file(dolt=db, table="menu_items", file_handle=open("./submited/" + filename, "r"), import_mode="create", commit=True, commit_message="Add data", do_continue=True)
@@ -227,16 +272,10 @@ with open("us_cities.csv", "r") as location_file:
                                 #         pass
 
             if last:
+                """Reset for next location"""
                 first = True
                 continue  # End iterating over restaurant_dict
                 time.sleep(2)
-
-
-
-
-
-
-
 
                 # print(json.dumps(parsed_response["restaurants"], indent=4))
                     # break
