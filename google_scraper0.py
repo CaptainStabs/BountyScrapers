d69cc86a825f56a1113c5ec403a84331682eecd1
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-08-01 17:40:42 -0400

    add upserve stuff

---
+++ google_scraper0.py
@@ -0,0 +1,31 @@
+import googlesearch as google
+from toasttab_function import toast_tab_scraper
+
+search_query = "allinanchor:  "/s/" site:app.upserve.com"
+ignored_domains = ["google"]
+remove_words = ["?mode=create"]
+print("   [*] Loop start")
+# with open("urls.txt", "a") as output:
+#     for results in google.search(search_query, tld="com", lang="en", num=10, start=0, stop=None, pause=2.0):
+#         # print("All result: " + results)
+#         if not any(ignored_domain in results for ignored_domain in ignored_domains):
+#             # print("   All result: " + results)
+#             print(results)
+#             try:
+#                 print("Scraping")
+#                 toast_tab_scraper(results)
+#             except exception:
+#                 print(exception)
+#                 pass
+
+
+for results in google.search(search_query, tld="com", lang="en", num=10, start=0, stop=None, pause=0.3):
+    # print("All result: " + results
+    # print("   All result: " + results)
+    print(results)
+    try:
+        print("Scraping")
+        toast_tab_scraper(results, skip_existing=True, pull_master=False,  local_branches=False)
+    except:
+        # print(exception)
+        pass
