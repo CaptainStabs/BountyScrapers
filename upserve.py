6d473787a50a648e518371a5ff9fb48159373174
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-08-01 18:41:34 -0400

    update

--- upserve.py
+++ upserve.py
@@ -1,8 +1,9 @@
 import requests
-import urllib.urlparse
+from urllib.parse import urlparse
+
 import json
 
-headers = json.dumps(headers = {
+headers = json.dumps({
     'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
     'DNT': '1',
     'sec-ch-ua-mobile': '?0',
@@ -13,7 +14,10 @@ headers = json.dumps(headers = {
     'Sec-Fetch-Dest': 'empty'
 })
 
-def upserve_scraper(webpage, headers):
+payload = {}
+
+
+def upserve_scraper(webpage, headers, payload):
     url = "https://d2evh2mef3r450.cloudfront.net/"
 
     parsed = urlparse(webpage) # Parse url
@@ -21,5 +25,9 @@ def upserve_scraper(webpage, headers):
 
     menu_url = url + web_path + ".json"
 
-    menu_response = requests.get(menu_url, headers=headers)
-    json.loads(menu_response.text)
+    menu_response = requests.request("POST", menu_url, headers=headers, data=payload)
+    print(menu_response.text)
+    menus_json = json.loads(menu_response.text)
+    print(json.dumps(menus_json, indent=4))
+
+upserve_scraper("https://app.upserve.com/s/bitter-and-twisted-cocktail-parlour-phoenix", headers=headers, payload=payload)
