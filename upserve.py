a05fd5aef508ac4803dde21d071c651ec56b8893
CaptainStabs <comportfailure@gmail.com>
2021-08-01 20:42:50 -0400

    merge master

--- upserve.py
+++ upserve.py
@@ -1,10 +1,11 @@
 import requests
-import urllib.urlparse
+from urllib.parse import urlparse
+
 import json
 
 headers = {
     'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
-    'DNT': '1',
+    'DNT':  '1',
     'sec-ch-ua-mobile': '?0',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
     'Accept': '*/*',
@@ -13,13 +14,20 @@ headers = {
     'Sec-Fetch-Dest': 'empty'
 }
 
-def upserve_scraper(webpage, headers):
-    url = "https://d2evh2mef3r450.cloudfront.net/"
+payload = {}
+
+
+def upserve_scraper(webpage, headers, payload):
+    url = "https://d2evh2mef3r450.cloudfront.net"
 
     parsed = urlparse(webpage) # Parse url
     web_path = parsed.path # Extract info from parse
 
-    menu_url = url + web_path + ".json"
+    menu_url = url + web_path + "/menu.json"
+    print(menu_url)
 
-    menu_response = requests.get(menu_url, headers=headers)
-    json.loads(menu_response.text)
+    menu_response = requests.request("GET", menu_url, headers=headers, data=payload)
+    menus_json = json.loads(menu_response.text)
+    # print(json.dumps(menus_json, indent=4))
+
+upserve_scraper("https://app.upserve.com/s/off-color-brewing-chicago", headers=headers, payload=payload)
