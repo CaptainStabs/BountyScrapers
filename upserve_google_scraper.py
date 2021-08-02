042b1a59eb0f1730709d3c6e47edfa9627a54eb4
CaptainStabs <comportfailure@gmail.com>
2021-08-02 14:23:01 -0400

    not entirely sure why i had dolt stuff in here

--- upserve_google_scraper.py
+++ upserve_google_scraper.py
@@ -7,30 +7,16 @@ from _cookie import dolt_cookie
 search_query = 'allinanchor:  "/s/" site:app.upserve.com'
 ignored_domains = ["google"]
 remove_words = ["?mode=create"]
-dolt_username = "captainstabs"
 
-payload = json.dumps({
-    "operationName": "PullsForRepo",
-    "variables": {
-        "query": f"{dolt_username}",
-        "ownerName": "dolthub",
-        "repoName": "menus"
-    },
-    "query": "query PullsForRepo($ownerName: String!, $repoName: String!, $pageToken: String, $filterByState: PullState, $query: String) {\n  pulls(\n    ownerName: $ownerName\n    repoName: $repoName\n    pageToken: $pageToken\n    filterByState: $filterByState\n    query: $query\n  ) {\n    ...PullListForPullList\n    __typename\n  }\n}\n\nfragment PullListForPullList on PullList {\n  list {\n    ...PullForPullList\n    __typename\n  }\n  nextPageToken\n  __typename\n}\n\nfragment PullForPullList on Pull {\n  _id\n  pullId\n  creatorName\n  title\n  fromBranchName\n  __typename\n}\n"
-})
-
-###  Headers for Dolt
 headers = {
     'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
-    'accept': '*/*',
-    'DNT': '1',
+    'DNT':  '1',
     'sec-ch-ua-mobile': '?0',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
-    'content-type': 'application/json',
-    'Sec-Fetch-Site': 'same-origin',
+    'Accept': '*/*',
+    'Sec-Fetch-Site': 'cross-site',
     'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Dest': 'empty',
-    'Cookie': f'{dolt_cookie}'
+    'Sec-Fetch-Dest': 'empty'
 }
 
 user_agent = google.get_random_user_agent()
@@ -44,7 +30,7 @@ with GracefulInterruptHandler() as h:
             break
         # print("All result: " + results
         # print("   All result: " + results)
-        print(results)
+        print("\n" + str(results))
         print("Scraping")
         upserve_scraper(results, headers, payload)
         # try:
