24d6a33db7c9f7a56a99120aae2b6ff826d4d5a0
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-08-18 13:54:04 -0400

    actually write the code for it

---
+++ 3_us_schools_bounty/clean_websites_data.py
@@ -0,0 +1,40 @@
+import csv
+import os
+import pandas as pd
+
+columns = ["name", "city", "state", "website"]
+
+with open("websites_added.csv", "r", encoding="utf-8") as input_source:
+    df = pd.read_csv(input_source)
+    df_columns = list(df.columns)
+    data_columns = ",".join(map(str, df_columns))
+
+    with open("cleaned_websites.csv", "a", encoding="utf-8") as output:
+        writer = csv.DictWriter(output, fieldnames=columns)
+
+        with open("dirty_websites.csv", "a", encoding="utf-8") as output2:
+            writer2 = csv.DictWriter(output2, fieldnames=columns)
+            if os.stat("dirty_websites.csv").st_size == 0:
+                writer.writeheader()
+
+            if os.stat("cleaned_websites.csv").st_size == 0:
+                writer.writeheader()
+
+            for index, row in df.iterrows():
+                if "wikipedia" not in row["website"]:
+                    output_dict = {}
+                    output_dict["name"] = row["name"]
+                    output_dict["city"] = row["city"]
+                    output_dict["state"] = row["state"]
+                    output_dict["website"] = row["website"]
+
+                    writer.writerow(output_dict)
+
+                else:
+                    output_dict = {}
+                    output_dict["name"] = row["name"]
+                    output_dict["city"] = row["city"]
+                    output_dict["state"] = row["state"]
+                    output_dict["website"] = ""
+
+                    writer2.writerow(output_dict)
