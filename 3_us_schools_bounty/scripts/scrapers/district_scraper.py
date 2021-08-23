e133e8b62ce81a9364c160e3b9f7776efce3af3f
CaptainStabs <comportfailure@gmail.com>
2021-08-23 10:54:37 -0400

    edited to open new verison

--- 3_us_schools_bounty/scripts/scrapers/district_scraper.py
+++ 3_us_schools_bounty/scripts/scrapers/district_scraper.py
@@ -13,21 +13,21 @@ from tqdm import tqdm
 columns = ["name", "city", "state", "district", "DISTRICTID"]
 
 headers = {
-  'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
-  'sec-ch-ua-mobile': '?0',
-  'Upgrade-Insecure-Requests': '1',
-  'DNT': '1',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
-  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-  'Sec-Fetch-Site': 'same-origin',
-  'Sec-Fetch-Mode': 'navigate',
-  'Sec-Fetch-User': '?1',
-  'Sec-Fetch-Dest': 'document',
+    "sec-ch-ua": '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+    "sec-ch-ua-mobile": "?0",
+    "Upgrade-Insecure-Requests": "1",
+    "DNT": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-User": "?1",
+    "Sec-Fetch-Dest": "document",
 }
 
 print("   [*] Opening Source File...")
 
-with open("districts_HIFLD_Schools.csv","r", encoding="utf-8") as input_source:
+with open("HIFLD_Schools.csv", "r", encoding="utf-8") as input_source:
     df = pd.read_csv(input_source)
     df_columns = list(df.columns)
     data_columns = ",".join(map(str, df_columns))
@@ -55,7 +55,9 @@ with GracefulInterruptHandler() as h:
 
             if no_error:
                 try:
-                    search_query = f"https://nces.ed.gov/ccd/districtsearch/district_list.asp?Search=1&details=1&InstName=&DistrictID={district_id}"
+                    search_query = (
+                        f"https://nces.ed.gov/ccd/districtsearch/district_list.asp?Search=1&details=1&InstName=&DistrictID={district_id}"
+                    )
                     response = requests.request("GET", search_query, headers=headers)
                     html_page = response.text
 
@@ -85,4 +87,4 @@ with GracefulInterruptHandler() as h:
                     school_info["DISTRICTID"] = row["DISTRICTID"]
                     writer.writerow(school_info)
 
-                    time.sleep(0.01)
+                    time.sleep(0.5)
