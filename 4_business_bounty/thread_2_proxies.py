0290e94a9ac953afcedde5c83f4b69b942115b2c
CaptainStabs <comportfailure@gmail.com>
2021-09-09 13:44:51 -0400

    update threat stuff

--- 4_business_bounty/thread_2_proxies.py
+++ 4_business_bounty/thread_2_proxies.py
@@ -2,7 +2,13 @@ from alabama_function_search import alabama_scraper_proxy
 from multiprocessing import Pool
 
 def run_parallel():
-    list_ranges = [[7630, 189199, "alabama1.csv", 1], [189199, 378399,"alabama2.csv", 2], [378399, 567599, "alabama3.csv", 3], [567599, 736799, "alabama4.csv", 4], [736799, 945999, "alabama5.csv", 5]]
+    one_start = 14776
+    two_start = 194671
+    three_start = 383882
+    four_start = 574507
+    five_start = 742313
+
+    list_ranges = [[one_start, 189199, "alabama1.csv", 1], [two_start, 378399,"alabama2.csv", 2], [three_start, 567599, "alabama3.csv", 3], [four_start, 736799, "alabama4.csv", 4], [five_start, 945999, "alabama5.csv", 5]]
     pool = Pool(processes=len(list_ranges))
     pool.map(alabama_scraper_proxy, list_ranges)
     # alabama_scraper()
