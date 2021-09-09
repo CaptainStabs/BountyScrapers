7e0c55b08757511d2d536b7275881d5b578d7ad0
CaptainStabs <comportfailure@gmail.com>
2021-09-09 11:28:44 -0400

    add user-agent headers

--- 4_business_bounty/alabama_search.py
+++ 4_business_bounty/alabama_search.py
@@ -5,14 +5,34 @@ import csv
 import os
 import time
 from tqdm import tqdm
+import random
+
 columns = ["name", "business_types", "state_registered","street_registered","city_registered","zip5_registered"]
+user_agents = [
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Mobile Safari/537.36'
+]
+stats = True
+def function_timer(stats):
+    if stats != False:
+        return time.perf_counter()
+
+
+# this function simply calculates and prints the difference between the end and start times
+def time_dif(stats, string, start, end):
+    if stats != False:
+        print(f"{string}: {end - start} seconds")
+
 
 with open("alabama.csv", "a") as output_file:
     writer = csv.DictWriter(output_file, fieldnames=columns)
 
     if os.stat("alabama.csv").st_size == 0:
         writer.writeheader()
-    for corp in tqdm(range(6593, 945999)):
+    for corp in tqdm(range(7630, 945999)):
         business_info = {}
         corp_padded = str(corp).zfill(6)
 
@@ -20,21 +40,30 @@ with open("alabama.csv", "a") as output_file:
 
 
         try:
+            request_start_time = function_timer(stats)
             url = f"https://arc-sos.state.al.us/cgi/corpdetail.mbr/detail?corp={corp_padded}&page=name&file=&type=ALL&status=ALL&place=ALL&city="
             print("  [*] Current business: " + str(corp_padded))
+            request_end_time = function_timer(stats)
+            time_dif(stats, "   [***] Request time", request_start_time, request_end_time)
             try:
-                response = requests.get(url)
+                user_agent = random.choice(user_agents)
+                headers = {'User-Agent': user_agent}
+                response = requests.get(url, headers=headers)
             except TimeoutError:
-                time.sleep(4)
+                time.sleep(1)
                 print("      [!] Timed out")
                 try:
-                    response = requests.get(url)
+                    user_agent = random.choice(user_agents)
+                    headers = {'User-Agent': user_agent}
+                    response = requests.get(url, headers=headers)
                 except TimeoutError:
-                    time.sleep(4)
+                    time.sleep(1)
                     print("      [!] Timed out again")
 
                     try:
-                        response = requests.get(url)
+                        user_agent = random.choice(user_agents)
+                        headers = {'User-Agent': user_agent}
+                        response = requests.get(url, headers=headers)
                     except TimeoutError:
                         print("      [!] Timed out third time, giving up.")
                         import sys
@@ -46,7 +75,9 @@ with open("alabama.csv", "a") as output_file:
                 time.sleep(4)
 
                 try:
-                    response = requests.get(url)
+                    user_agent = random.choice(user_agents)
+                    headers = {'User-Agent': user_agent}
+                    response = requests.get(url, headers=headers)
                 except requests.exceptions.ConnectionError:
                     sys.exit()
 
@@ -55,6 +86,7 @@ with open("alabama.csv", "a") as output_file:
 
 
             # print(response.status_code)
+            parse_start = function_timer(stats)
             html_page = response.text
             soup = BeautifulSoup(html_page, "html.parser")
             name = soup.find("thead").find("tr").find("td").get_text()
@@ -64,16 +96,23 @@ with open("alabama.csv", "a") as output_file:
             business_info["name"] = name
             business_info["state_registered"] = state
             rows = soup.find("table").find_all("tr")
+            parse_end = function_timer(stats)
+            time_dif(stats, "   [***] Parse time", parse_start, parse_end)
             i = 0
+
             for row in rows:
                 # do_save = False
 
                 rows_data = row.find_all("td", class_="aiSosDetailValue")
+                row_data_start = function_timer(stats)
                 for row_data in rows_data:
+                    finished = False
                     # print(row_data)
                     # if row_data.has_attr("aiSosDetailValue"):
-                    print(i)
+                    # print(i)
+
                     if i == 1:
+                        business_parse_start = function_timer(stats)
                         business_type_string = str(row_data.get_text()).upper()
                         print("      [*] Bussiness type: " + business_type_string)
                         if "COOPERATIVE" in business_type_string:
@@ -124,6 +163,29 @@ with open("alabama.csv", "a") as output_file:
                             business_info["business_types"] = "TRUST"
                             print("      [?] Translated type: TRUST")
 
+                        if "INC " in business_type_string:
+                            business_info["business_types"] = "CORPORATION"
+                            print("      [?] Translated type 1: INC"))
+
+                        if "INC" in business_type_string:
+                            business_info["business_types"] = "CORPORATION"
+                            print("      [?] Translated type 2: INC")
+
+                        if "INCORPORATED" in business_type_string:
+                            business_info["business_types"] = "CORPORATION"
+                            print("      [?] Translated type 3: INC")
+
+                        if "LTD" in business_type_string:
+                            business_info["business_types"] = "LTD"
+                            print("      [?] Translated type 1: LTD")
+
+                        if "L.T.D" in business_type_string:
+                            business_info["business_types"] = "LTD"
+                            print("      [?] Translated type 1: LTD")
+                        business_parse_end = function_timer(stats)
+                        time_dif(stats, "   [***] Business type parsing", business_parse_start, business_parse_end)
+
+
                     if i == 2:
                         address_string = str(row_data.get_text()).upper()
                         try:
@@ -164,19 +226,27 @@ with open("alabama.csv", "a") as output_file:
                             print("      [*] Exists: " + str(row_data.get_text()).upper())
                             do_save = True
                     if i <= 4:
-                        print("increment i ")
+                        # print("increment i ")
                         i += 1
                     else:
                         if do_save:
                             print("      [*] Saving")
                             writer.writerow(business_info)
                             do_save = False
+                            finished = True
                             break
                             break
                         else:
-                            print("       [*] Not saving")
+                            # print("       [*] Not saving")
+                            finished = True
                             break
                             break
+
+
+            if finished:
+                row_data_end = function_timer(stats)
+                time_dif(stats, "   [***] Row_data loop time", row_data_start, row_data_end)
+                break
 
 
 
--- 4_business_bounty/thread_1.py
+++ 4_business_bounty/thread_1.py
@@ -2,9 +2,22 @@ from alabama_function_search import alabama_scraper
 from multiprocessing import Pool
 
 def run_parallel():
-    list_ranges = [[6593, 189199, "alabama1.csv", 1], [189199, 378399,"alabama2.csv", 2], [378399, 567599, "alabama3.csv", 3], [567599, 736799, "alabama4.csv", 4], [736799, 945999, "alabama5.csv", 5]]
-    pool = Pool(processes=len(list_ranges))
-    pool.map(alabama_scraper, list_ranges)
+    one_start = 9205
+    two_start = 190678
+    three_start = 379998
+    four_start = 569073
+    five_start = 738402
+
+    list_ranges = [[one_start, 189199, "alabama1.csv", 1], [two_start, 378399,"alabama2.csv", 2], [three_start, 567599, "alabama3.csv", 3], [four_start, 736799, "alabama4.csv", 4], [five_start, 945999, "alabama5.csv", 5]]
+    try:
+        pool = Pool(processes=len(list_ranges))
+        pool.map(alabama_scraper, list_ranges)
+
+    except KeyboardInterrupt:
+        print("Quitting")
+        pool.terminate()
+        sys.exit(1)
     # alabama_scraper()
+
 if __name__ == "__main__":
-    run_parallel()
+        run_parallel()
