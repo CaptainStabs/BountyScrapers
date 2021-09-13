72b770f6694c137e483a9c9b20b3b5d8d7046f8f
CaptainStabs <comportfailure@gmail.com>
2021-09-13 08:34:08 -0400

    add alabama stuff

---
+++ 4_business_bounty/proxy_threads.bat
@@ -0,0 +1,22 @@
+@echo OFF
+rem How to run a Python script in a given conda environment from a batch file.
+
+rem It doesn't require:
+rem - conda to be in the PATH
+rem - cmd.exe to be initialized with conda init
+
+rem Define here the path to your conda installation
+set CONDAPATH=C:\Users\adria\miniconda3
+rem Define here the name of the environment
+set ENVNAME=scraper
+
+rem The following command activates the base environment.
+rem call C:\Users\adria\miniconda3\condabin\activate.bat C:\Users\adria\miniconda3
+if %ENVNAME%==base (set ENVPATH=%CONDAPATH%) else (set ENVPATH=%CONDAPATH%\envs\%ENVNAME%)
+
+rem Activate the conda environment
+rem Using call is required here, see: https://stackoverflow.com/questions/24678144/conda-environments-and-bat-files
+call %CONDAPATH%\Scripts\activate.bat %ENVPATH%
+
+rem Run a python script in that environment
+python thread_2_proxies.py
