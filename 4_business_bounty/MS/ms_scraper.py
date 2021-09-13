0f41e2228b40d06abbd2bf41aa160ecb1fa6515e
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-09-13 14:40:09 -0400

    add missispi scraper

---
+++ 4_business_bounty/MS/ms_scraper.py
@@ -0,0 +1,231 @@
+import requests
+import csv
+import os
+import sys
+from lxml.html import fromstring
+from lxml.cssselect import CSSSelector
+import usaddress
+from tqdm import tqdm
+import pandas as pd
+import json
+import random
+import re
+
+# Get random user agent
+def get_user_agent():
+    user_agents = [
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+	'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+	'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+	'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Mobile Safari/537.36'
+    ]
+
+    user_agent = random.choice(user_agents)
+    headers = {
+        'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'DNT': '1',
+        'X-Requested-With': 'XMLHttpRequest',
+        'sec-ch-ua-mobile': '?0',
+        'User-Agent': user_agent,
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+    }
+    return headers
+
+# Get Business_Id
+def get_ids(business_id):
+    url = "https://corp.sos.ms.gov/corp/Services/MS/CorpServices.asmx/BusinessIdSearch"
+
+    payload = {"BusinessId":f"{business_id}"}
+    payload = str(payload).replace("'", '"')
+
+    response = requests.request("POST", url, headers=get_user_agent(), data=payload)
+
+    response_json = json.loads(response.text)
+    # print(json.dumps(response_json, indent=4))
+    d_json = json.loads(response_json["d"])
+    # print(d_json["Table"][0]["FilingId"])
+    filing_id = d_json["Table"][0]["FilingId"]
+    return filing_id
+
+# Get the info page
+def get_info(filing_id, writer):
+    business_info = {}
+    url = f"https://corp.sos.ms.gov/corp/portal/c/page/corpbusinessidsearch/~/ViewXSLTFileByName.aspx?providerName=MSBSD_CorporationBusinessDetails&FilingId={filing_id}"
+    response = requests.request("GET", url, headers=get_user_agent())
+
+    parser = fromstring(response.text)
+
+    business_status = str(parser.xpath('//*[@id="printDiv2"]/table[1]/tr[3]/td[2]/text()')[0]).upper().replace("  ", " ").strip()
+    if business_status == "GOOD STANDING":
+        print("   [*] Status: Good Standing")
+        name = str(parser.xpath('//*[@id="printDiv2"]/div[2]/table/tr[2]/td[1]/text()')[0]).upper().replace("  ", " ").strip()
+        print("   [*] Name: " + name)
+        business_type_string = str(parser.xpath('//*[@id="printDiv2"]/table[1]/tr[1]/td[2]/text()')[0]).upper().replace("  ", " ").strip()
+        print("   [*] Business Type String: " + business_type_string)
+
+        if "COOPERATIVE" in business_type_string:
+            business_info["business_type"] = "COOP"
+            print("      [?] Translated type 1: COOP")
+
+        if "COOP " in business_type_string:
+            business_info["business_type"] = "COOP"
+            print("      [?] Translated type 2: COOP")
+        if "CORP" in business_type_string:
+            business_info["business_type"] = "CORPORATION"
+            print("      [?] Translated type 1: CORPORATION")
+
+        if "CORP " in business_type_string:
+            business_info["business_type"] = "CORPORATION"
+            print("      [?] Translated type 2: CORPORATION")
+
+        if "CORPORATION" in business_type_string:
+            business_info["business_type"] = "CORPORATION"
+            print("      [?] Translated type 3: CORPORATION")
+
+        if "DBA" in business_type_string:
+            business_info["business_type"] = "DBA"
+            print("      [?] Translated type: DBA")
+
+        if "LIMITED LIABILITY COMPANY" in business_type_string:
+            business_info["business_type"] = "LLC"
+            print("      [?] Translated type 1: LLC")
+
+        if "LLC" in business_type_string:
+            business_info["business_type"] = "LLC"
+            print("      [?] Translated type 2: LLC")
+
+        if "L.L.C." in business_type_string:
+            business_info["business_type"] = "LLC"
+            print("      [?] Translated type 3: LLC")
+
+        if "L.L.C" in business_type_string:
+            business_info["business_type"] = "LLC"
+            print("      [?] Translated type 4: LLC")
+
+        if "NON-PROFIT" in business_type_string:
+            business_info["business_type"] = "NONPROFIT"
+            print("      [?] Translated type 1: NON-PROFIT")
+
+        if "NONPROFIT" in business_type_string:
+            business_info["business_type"] = "NONPROFIT"
+            print("      [?] Translated type 2: NONPROFIT")
+
+        if "PARTNERSHIP" in business_type_string:
+            business_info["business_type"] = "PARTNERSHIP"
+            print("      [?] Translated type: PARTNERSHIP")
+
+        if "SOLE PROPRIETORSHIP" in business_type_string:
+            business_info["business_type"] = "SOLE PROPRIETORSHIP"
+            print("      [?] Translated type: SOLE PROPRIETORSHIP")
+
+        if "TRUST" in business_type_string:
+            business_info["business_type"] = "TRUST"
+            print("      [?] Translated type: TRUST")
+
+        if "INC " in business_type_string:
+            business_info["business_type"] = "CORPORATION"
+            print("      [?] Translated type 1: INC")
+
+        if "INC" in business_type_string:
+            business_info["business_type"] = "CORPORATION"
+            print("      [?] Translated type 2: INC")
+
+        if "INCORPORATED" in business_type_string:
+            business_info["business_type"] = "CORPORATION"
+            print("      [?] Translated type 3: INC")
+
+        # if "LIMITED" in business_type_string:
+        #     business_info["business_type"] = "LTD"
+        #     print("      [?] Translaetd type1: LTD")
+
+        if "LTD" in business_type_string:
+            business_info["business_type"] = "LTD"
+            print("      [?] Translaetd type 2: LTD")
+
+        if "L.T.D" in business_type_string:
+            business_info["business_type"] = "LTD"
+            print("      [?] Translaetd type 3: LTD")
+
+        filing_number = str(parser.xpath('//*[@id="printDiv2"]/table[1]/tr[2]/td[2]/text()')[0]).strip()
+        address_string = str(parser.xpath('//*[@id="printDiv2"]/table[1]/tr[6]/td[2]/text()')).upper().replace("  ", " ").strip().replace("\\XA0\\R\\N", "").replace("\\XA0", "")
+        # if address_string != "[]":
+        #     is_address = True
+        #     address_list = address_string.strip("['']").split(",")
+        #     state_zip = address_list[-1]
+        #     print(address_list)
+        #     print(state_zip)
+        #     temp = re.compile("([a-zA-Z]+)([0-9]+)")
+        #     res = temp.match(state_zip).groups()
+        #     print(res)
+        # else:
+        #     is_address = False
+
+        try:
+            street_address = str(parser.xpath('//*[@id="printDiv2"]/table[1]/tr[6]/td[2]/text()[1]')[0]).upper().replace("  ", " ").strip()
+        except IndexError:
+            street_address = str(parser.xpath('//*[@id="printDiv2"]/table[1]/tr[6]/td[2]/text()[1]')).upper().replace("  ", " ").strip()
+        city_state_zip = str(parser.xpath('//*[@id="printDiv2"]/table[1]/tr[6]/td[2]/text()[2]')).upper().replace("  ", " ").strip().replace("\\XA0\\R\\N", "").replace("\\XA0", "").strip("[]").join(",")
+
+
+        if address_string != "NO PRINCIPAL OFFICE ADDRESS FOUND":
+            business_info["street_physical"] = street_address
+            # try:
+            #     print("   [*] Address " + address_string)
+            #     parsed_address = usaddress.tag(city_state_zip)
+            #     parse_success = True
+            #
+            # except usaddress.RepeatedLabelError as e:
+            #     print(e)
+            #     parse_success = False
+            #
+            # if parse_success:
+            #     # street_registered = f'{parsed_address[0]["AddressNumber"]} {parsed_address[0]["StreetName"]} {parsed_address[0]["StreetNamePostType"]}'
+            #     street_registered = city_state_zip
+            #     print(parsed_address)
+            #     business_info["street_physical"] = street_registered
+            #
+            #     try:
+            #         business_info["city_physical"] = parsed_address[0]["PlaceName"]
+            #         business_info["zip5_physical"] = parsed_address[0]["ZipCode"]
+            #         parse_success = True
+            #
+            #     except KeyError:
+            #         pass
+
+
+
+        business_info["name"] = name
+        business_info["filing_number"] = filing_number
+        business_info["corp_id"] = business_id
+
+        try:
+            incorporation_state = str(parser.xpath('//*[@id="printDiv2"]/table[1]/tr[5]/td[2]/text()')[0]).upper().replace("  ", " ").strip()
+            business_info["state_registered"] = incorporation_state
+            success = True
+
+        except KeyError:
+            print(f"   [!!!] KeyError! State: '{incorporation_state}' is not in dictionary!")
+            success = False
+            pass
+
+        if success:
+            writer.writerow(business_info)
+    else:
+        print("   [*] Status: " + business_status)
+
+filename = "mississippi.csv"
+columns = ["name", "business_type", "state_registered","street_physical","city_physical","zip5_physical", "filing_number", "corp_id"]
+with open(filename, "a", encoding="utf-8") as output_file:
+    writer = csv.DictWriter(output_file, fieldnames=columns)
+
+    if os.stat(filename).st_size == 0:
+        writer.writeheader()
+
+    # business_id = 1073778
+    for business_id in tqdm(range(1000001, 1047088)):
+        get_info(get_ids(business_id), writer)
