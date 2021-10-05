3fe6c27edb67d3af9ce6e7191a605ef7861188ef
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-10-05 15:28:41 -0400

    update sec

--- 4_business_bounty/sec/data_extractor.py
+++ 4_business_bounty/sec/data_extractor.py
@@ -14,4 +14,31 @@ with open(filename, "a", encoding="utf-8", newline="") as output:
                     break
 
                 loaded_json = json.load(file)
-                
+                if loaded_json["entityType"] == "operating":
+                    print("   [*] Entity is active!")
+
+                    business_info = {}
+
+                    sic4 = loaded_json["sic"]
+                    ein = loaded_json["ein"]
+                    website = loaded_json["website"]
+                    state_registered = loaded_json["stateOfIncorporation"]
+
+                    mailing_address = loaded_json["mailing"]
+                    street1 = mailing_address["street1"]
+                    street2 = mailing_address["street2"]
+
+                    city = mailing_address["city"]
+                    state = mailing_address["StateOrCountry"]
+                    zipcode = mailing_address["zipCode"]
+
+                    if "-" in zipcode:
+                        zipcode = zipcode.split("-")[0]
+
+                    
+
+                elif loaded_json["entityType"] == "other":
+                    print("   [*] Entity is not active!")
+                else:
+                    with open("entityType.txt", "a", newline="") as fail_file:
+                        fail_file.write(str(loaded_json["entityType"]) + "\n")
