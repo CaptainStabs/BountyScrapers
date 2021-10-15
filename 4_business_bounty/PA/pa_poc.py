51392f6809dd4258eda92469c956562dcf915ab3
CaptainStabs <comportfailure@gmail.com>
2021-10-15 08:40:35 -0400

    add more PA stuff

--- 4_business_bounty/PA/pa_poc.py
+++ 4_business_bounty/PA/pa_poc.py
@@ -1,5 +1,7 @@
 import requests
 from lxml.html import fromstring
+import pandas as pd
+import usaddress
 
 headers = {
     'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
@@ -16,9 +18,13 @@ headers = {
     'Sec-Fetch-Dest': 'document'
 }
 
+'''
+Step 1:
+Need to get the first search page to search for ID
+'''
 # Get search page
 url = "https://www.corporations.pa.gov/search/corpsearch"
-response = requests.request("GET", url)
+response = requests.request("GET", url, headers=headers)
 
 # Parse raw html with lxml
 parser = fromstring(response.text)
@@ -28,7 +34,6 @@ view_state = parser.xpath('//*[@id="__VIEWSTATE"]/@value')[0]
 
 # Setup payload
 payload = {
-    '__EVENTARGUMENT': '',
     '__EVENTTARGET': '',
     '__EVENTVALIDATION': event_validation,
     '__VIEWSTATE': view_state,
@@ -37,30 +42,44 @@ payload = {
     'ctl00$MainContent$txtSearchTerms': '710774'
 }
 
+
 # Get "Select Business Entity page"
 result_page = requests.request("POST", url, data=payload)
-
-with open("test.html", "w") as f:
-    f.write(result_page.text)
 
 # Get view stuff from this page, and buttons daat
 result_parser = fromstring(result_page.text)
 
-# Get aspx junk
-event_validation = parser.xpath('//*[@id="__EVENTVALIDATION"]/@value')[0]
-view_state = parser.xpath('//*[@id="__VIEWSTATE"]/@value')[0]
+# Get aspx junk for step 2 request
+event_validation_2 = result_parser.xpath('//*[@id="__EVENTVALIDATION"]/@value')[0]
+view_state_2 = result_parser.xpath('//*[@id="__VIEWSTATE"]/@value')[0]
+
+'''
+Step 2:
+After searching, we need to get the view_state and event_validation once again
+EVENTTARGET is from the result list link pointing to entity name
+This step (finally) gets the actual business info
+'''
 
 payload = {
     '__EVENTARGUMENT': '',
     '__EVENTTARGET': 'ctl00$MainContent$gvResults$ctl02$lnkBEName',
-    '__EVENTVALIDATION': event_validation,
-    '__VIEWSTATE': view_state
+    '__EVENTVALIDATION': event_validation_2,
+    '__VIEWSTATE': view_state_2
 }
 
+
 business_page = requests.request("POST", url, headers=headers, data=payload)
-with open("response")
 business_parser = fromstring(business_page.text)
 
+with open("test.html", 'w') as f:
+    f.write(business_page.text)
 
-print("Name: " + str(business_parser.xpath('//*[@id="gvBusinessNameHistory"]/tr/td[1]/text()')))
-print(business_parser.xpath('//*[@id="MainContent_lblTitleOrder"]/text()'))
+print("Name: " + str(business_parser.xpath('//td[@align="left"]/text()')[0]).strip())
+print(business_parser.xpath('/html/body/div/form/div[5]/div[2]/div[3]/div/div[5]/div[1]/table/tr[1]/td[2]/text()'))
+# //*[@id="MainContent_pnlEntityDetails"]/div[3]/div/div[5]/div[1]/table/tbody/tr[3]/td[2]
+df = pd.read_html(business_page.text)
+entity_details = df[1]
+info_dict = entity_details.set_index(0).to_dict()
+print(info_dict)
+raw_registered_address = info_dict[1]['Address']
+print(raw_registered_address)
---
+++ 4_business_bounty/PA/temp.py
@@ -0,0 +1,4 @@
+from utils.business_parser import business_type_parser
+
+business_type_string = "PARTNERSHIPLLC ADSFASF ASFASFAS"
+print(business_type_parser(business_type_string))
