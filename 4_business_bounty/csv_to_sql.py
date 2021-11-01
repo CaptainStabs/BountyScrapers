12c5b29807a658d5aee50a9de4746cd6a2941042
CaptainStabs <comportfailure@gmail.com>
2021-11-01 14:53:17 -0400

    update stuff

--- 4_business_bounty/csv_to_sql.py
+++ 4_business_bounty/csv_to_sql.py
@@ -2,20 +2,20 @@ import csv
 from tqdm import tqdm
 
 # General settings
-filename = "C:\\Users\\adria\\github\\BountyScrapers\\4_business_bounty\\AR\\arkansas_3.csv"
+filename = "C:\\Users\\adria\\github\\BountyScrapers\\4_business_bounty\\MO\\mt_cleaned_merged_data.csv"
 table_name = "businesses"
 
 # SQL Update Settings
 pk = ["name", "business_type", "state_registered"]
-updated_columns = ["street_physical"]
-fieldnames = ["name",	"business_type",	"state_registered",	"street_registered",	"city_registered",	"zip5_registered",	"state_physical",	"street_physical",	"city_physical",	"zip5_physical",	"filing_number",	"public",	"naics_2017",	"ein",	"sic4",	"parent",	"website", "duns"]
+updated_columns = ["street_registered",	"city_registered",	"zip5_registered",	"state_physical",	"street_physical",	"city_physical",	"zip5_physical",	"filing_number"]
+fieldnames = ["name",	"business_type",	"state_registered",	"street_registered",	"city_registered",	"zip5_registered",	"state_physical",	"street_physical",	"city_physical",	"zip5_physical",	"filing_number"]#,	"public",	"naics_2017",	"ein",	"sic4",	"parent",	"website", "duns"]
 
 output_filename = str(filename).strip(".csv") + "_sql.sql"
 print(output_filename)
 
 def csv_to_insert(filename, table_name):
     # Count lines in file for tqdm total
-    with open(filename, "r") as input_csv:
+    with open(filename, "r", encoding="utf-8") as input_csv:
         line_count = 0
         # Count lines in file
         for line in input_csv.readlines():
@@ -32,7 +32,6 @@ def csv_to_insert(filename, table_name):
                 # Get the header and parse to fields
                 if index == 0:
                     fields = ",".join(row)
-                    # print(fields)
 
                 # Every other line
                 else:
@@ -40,7 +39,7 @@ def csv_to_insert(filename, table_name):
                     query_statement = f"INSERT INTO {table_name}({fields}) VALUES ({values});\n"
                     output_file.write(query_statement)
 
-def csv_to_update(filename, pk, updated_columns, fieldnames):
+def csv_to_update(filename, table_name, pk, updated_columns, fieldnames):
     with open(filename, "r", encoding="utf-8") as input_csv:
         line_count = 0
         # Count lines in file
@@ -65,8 +64,14 @@ def csv_to_update(filename, pk, updated_columns, fieldnames):
                 else:
 
                     values = str(row).strip("[]")
-                    query_statement = f"UPDATE {table_name} SET {updated_columns[0]} = '{row['street_physical']}' WHERE {pk[0]} = '{row['name']}' and {pk[1]} = '{row['business_type']}' and {pk[2]} = '{row['state_registered']}';\n"
+                    set_statement_list = []
+                    for i in range(len(updated_columns)):
+                        column_to_update = updated_columns[i]
+                        set_statement_list.append(f"{column_to_update} = '{row[column_to_update]}'")
+
+                    set_statements = ",".join(set_statement_list)
+                    query_statement = f"UPDATE {table_name} SET " + str(set_statements) + f" WHERE {pk[0]} = '{row['name']}' and {pk[1]} = '{row['business_type']}' and {pk[2]} = '{row['state_registered']}';\n"
                     output_file.write(query_statement)
 
-# csv_to_update(filename, pk, updated_columns, fieldnames)
+# csv_to_update(filename, table_name, pk, updated_columns, fieldnames)
 csv_to_insert(filename, table_name)
