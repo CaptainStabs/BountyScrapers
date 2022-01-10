b52fb3b8bc28a5e1aa38b3c9f105d62c0ab9444d
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-01-10 15:23:33 -0500

    add edge cases to "parser"

--- 6_us_housing_prices/detroit/data_extractor.py
+++ 6_us_housing_prices/detroit/data_extractor.py
@@ -10,6 +10,12 @@ with open("Property_Sales.csv", 'r') as f:
         writer = csv.DictWriter(output, fieldnames=columns)
         writer.writeheader()
         for i, row in tqdm(enumerate(read_csv)):
+            try:
+                del book
+                del page
+            except:
+                pass
+
             if i:
                 if row[6]:
                     land_info = {
@@ -18,47 +24,73 @@ with open("Property_Sales.csv", 'r') as f:
                         "sale_date": str(parser.parse(str(row[4])))[:-6]
                     }
 
+
                     book = row[6]
 
-                    if "/" not in book:
-                        if " " in book:
-                            book = row[6].split(" ")[0].lstrip("L")
-                            page = row[6].split(" ")[1].lstrip("P")
-                        elif ":" in book:
-                            book = row[6].split(":")[0]
-                            page = row[6].split(":")[1]
+                    try:
+                        if "/" not in book:
+                            if "LIBER" in book:
+                                # print(book)
+                                book = row[6].split("PAGE")[0].replace("LIBER ", "").strip()
+                                page = row[6].split("PAGE")[1].strip().lstrip(":").lstrip("P:").lstrip("P").strip()
 
-                        elif "-" in book:
-                            book = row[6].split("-")[0]
-                            page = row[6].split("-")[1]
 
-                        elif ";" in book:
-                            book = row[6].split(";")[0]
-                            page = row[6].split(";")[1]
+                            elif "L:" and "P:" in book:
+                                book = str(row[6].split("P:")[0]).lstrip("L:").strip()
+                                page = row[6].split("P:")[1].lstrip(":").lstrip("P:").lstrip("P").strip()
 
-                        elif "L" and "P" in book:
-                            book = str(row[6].split("P")[0]).lstrip("L")
-                            page = str(row[6].split("P")[1])
+                            elif ", " in book:
+                                book = str(row[6].split(", ")[0]).lstrip("L").rstrip(",").strip()
+                                page = str(row[6].split(", ")[1]).lstrip("P").lstrip(":").strip()
 
-                        elif ", " in book:
-                            book = str(row[6].split(", ")[0]).lstrip("L")
-                            page = str(row[6].split(", ")[1]).lstrip("P")
+                            elif ":" in book:
+                                book = row[6].split(":")[0].strip().lstrip("L").lstrip(":").lstrip("L:").strip()
+                                page = row[6].split(":")[1].lstrip(":").lstrip("P:").lstrip("P").strip()
 
-                        # else:
-                        #     if len(row[6]) != 10:
-                        #         print(row[6])
-                    else:
-                        book = row[6].split("/")[0]
+                            elif "-" in book:
+                                book = row[6].split("-")[0].lstrip("L").lstrip(":").lstrip("L:").strip()
+                                page = row[6].split("-")[1].lstrip(":").lstrip("P:").lstrip("P").strip()
+
+                            elif ";" in book:
+                                book = row[6].split(";")[0].lstrip("L").lstrip(":").lstrip("L:").strip()
+                                page = row[6].split(";")[1].lstrip(":").lstrip("P:").lstrip("P").strip()
+
+
+                            elif "L" and "P" in book:
+                                book = str(row[6].split("P")[0]).lstrip("L").strip()
+                                page = str(row[6].split("P")[1]).lstrip(":").lstrip("P:").strip()
+
+                            elif " " in book:
+                                book = row[6].split(" ")[0].lstrip("L").strip()
+                                page = row[6].split(" ")[1].lstrip("P").lstrip(":").lstrip("P:").strip()
+
+
+
+                            # else:
+                            #     if len(row[6]) != 10:
+                            #         print(row[6])
+                        elif "/" in book:
+                            book = row[6].split("/")[0].strip().lstrip("L").lstrip(":").lstrip("L:").strip()
+
+                            try:
+                                page = row[6].split("/")[1].strip().lstrip("P").lstrip(":").lstrip("P:").strip()
+                            except IndexError:
+                                pass
+                                print(row[6])
+                        else:
+                            print(row[6])
 
                         try:
-                            page = row[6].split("/")[1]
-                        except IndexError:
+                            if "-" in page:
+                                page = page.split("-")[0]
+
+                            land_info["book"] = book
+                            land_info["page"] = page
+                            writer.writerow(land_info)
+
+                        except NameError:
                             pass
-                            # print(row[6])
-
-                    if "-" in page:
-                        page = page.split("-")[0]
-
-                    land_info["book"] = book
-                    land_info["page"] = page
-                    writer.writerow(land_info)
+                            # if len(row[6]) != 10:
+                            #     print(row[6])
+                    except IndexError:
+                        print(book)
