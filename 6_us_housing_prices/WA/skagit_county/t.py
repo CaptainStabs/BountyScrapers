03d90d45dc10286b20c23bda0199955b92019a2c
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-01-12 14:25:43 -0500

    add columbus GA testing stuff

--- 6_us_housing_prices/WA/skagit_county/t.py
+++ 6_us_housing_prices/WA/skagit_county/t.py
@@ -2,4 +2,4 @@ from dateutil import parser
 
 print(parser.parse('1991-12-26'))
 
-print(str('                    $20,500.00').strip().replace("$", "").replace(",", "").split(".")[0])
+print(str('                    $.00').strip().replace("$", "").replace(",", "").split(".")[0])
