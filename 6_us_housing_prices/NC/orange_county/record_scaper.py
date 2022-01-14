efccbc7665ea66d8d2786178317b9c0ad2414f42
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-01-14 15:27:13 -0500

    uodatea

--- 6_us_housing_prices/NC/orange_county/record_scaper.py
+++ 6_us_housing_prices/NC/orange_county/record_scaper.py
@@ -14,8 +14,25 @@ response = requests.request("GET", url, headers=headers, data=payload)
 cleaned_response = str(response.text).replace("\\", "").strip('"')
 json_data = json.loads(cleaned_response)
 
+sale_data = parcel["sections"][3][0][0]
+
 land_info = {
-    "state":"NC"
+    "state": "NC",
+    "county": "ORANGE COUNTY",
+    "sale_date": sale_data["order_0"],
+    "book": sale_data["Book"],
+    "page": sale_data["Page"],
+
 }
 
-print(json.dumps(json_data, indent=2))
+if sale_data["seller_name"] != "null":
+    land_info["seller_name"] = sale_data["seller_name"]
+
+if sale_data["SalePrice"] != "null":
+    # save
+    land_info["sale_price"] = sale_data["SalePrice"]
+
+parcel = json_data["parcel"]
+sale_date = parcel["sections"][3][0][0]["order_0"]
+# print(json.dumps(json_data, indent=2))
+print(sale_date)
