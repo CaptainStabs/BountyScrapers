ed30c8ef0a3e9d95d4d8dbbd81e5583bf5a35917
CaptainStabs <comportfailure@gmail.com>
2022-01-16 13:34:28 -0500

    Add Marion OR

--- 6_us_housing_prices/OR/marion/0_data_extractor.py
+++ 6_us_housing_prices/OR/marion/0_data_extractor.py
@@ -2,11 +2,13 @@ import os
 import csv
 from tqdm import tqdm
 from dateutil import parser
+import usaddress
+
 
 directory = ".\\files\\0\\"
 
 # PROPID,SALEPRICE,SALEDATE,SITUSNUMBER,SITUSSTREET,SITUSCITYSTATEZIP,SELLERNAME,BUYERNAME,YEARBUILT,
-columns = ["property_id", "sale_price", "sale_date", "physical_address", "city", "state", "zip5", "seller_name", "buyer_name", "year_built"]
+columns = ["property_id", "sale_price", "sale_date", "physical_address", "city", "state", "zip5", "seller_name", "buyer_name", "year_built", "source_url"]
 
 with open("0_extracted_data.csv", "a", encoding="utf-8", newline="") as output_csv:
     writer = csv.DictWriter(output_csv, fieldnames=columns)
@@ -21,6 +23,7 @@ with open("0_extracted_data.csv", "a", encoding="utf-8", newline="") as output_c
                     "property_id": row["PROPID"],
                     "seller_name": " ".join(str(row["SELLERNAME"]).upper().strip().split()),
                     "buyer_name": " ".join(str(row["BUYERNAME"]).upper().strip().split()),
+                    "source_url": "https://www.co.marion.or.us/AO/Pages/datacenter.aspx"
                 }
 
                 street_list = [str(row["SITUSNUMBER"]).strip(), str(row["SITUSSTREET"]).strip()]
@@ -28,16 +31,37 @@ with open("0_extracted_data.csv", "a", encoding="utf-8", newline="") as output_c
                 # concat the street parts filtering out blank parts
                 land_info["physical_address"] = ' '.join(str(' '.join(filter(None, street_list)).upper()).split())
 
-                city_state_zip = str(row["SITUSCITYSTATEZIP"]).split(" ")
-                if len(city_state_zip) > 1:
-                    print(city_state_zip, len(city_state_zip))
-                    land_info["city"] = city_state_zip[0].strip().rstrip(",")
-                    land_info["state"] = city_state_zip[1].strip()
+                city_state_zip = row["SITUSCITYSTATEZIP"]
 
-                    if "-" in city_state_zip[2]:
-                        land_info["zip5"] = str(city_state_zip[2]).split("-")[0]
-                    else:
-                        land_info["zip5"] = city_state_zip[2].strip()
+                try:
+                    parsed_address = usaddress.tag(city_state_zip)
+
+                except usaddress.RepeatedLabelError as e:
+                    print(e)
+
+                try:
+                    land_info["city"] = " ".join(str(parsed_address[0]["PlaceName"]).strip().rstrip(",").upper().split())
+                except KeyError:
+                    # print("Error on city:", city_state_zip)
+                    pass
+
+                try:
+                    land_info["state"] = str(parsed_address[0]["StateName"]).upper().strip()
+
+                except KeyError:
+                    # print("Error on state:", city_state_zip)
+                    pass
+
+                try:
+                    zip5 = str(parsed_address[0]["ZipCode"]).strip()
+
+                    if "-" in zip5:
+                        zip5 = zip5.split("-")[0]
+
+                    land_info["zip5"] = zip5
+                except KeyError:
+                    # print("Error on zip:", city_state_zip)
+                    pass
 
                 sale_date = row["SALEDATE"]
 
@@ -51,6 +75,8 @@ with open("0_extracted_data.csv", "a", encoding="utf-8", newline="") as output_c
 
                 if land_info["seller_name"] == "MISSING SELLER RECORD":
                     land_info["seller_name"] = ""
+
+                land_info["year_built"] = row["YEARBUILT"].split(".")[0]
 
                 if land_info["sale_price"] and land_info["sale_date"] and land_info["physical_address"]:
                     writer.writerow(land_info)
