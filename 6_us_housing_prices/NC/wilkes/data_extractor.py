cca436d73cd535d74b52dec9ef601448cb3d0ae5
CaptainStabs <comportfailure@gmail.com>
2022-01-17 15:27:11 -0500

    add wilkes county NC data extractor

---
+++ 6_us_housing_prices/NC/wilkes/data_extractor.py
@@ -0,0 +1,30 @@
+import csv
+from tqdm import tqdm
+
+# PARCEL_ID,BK,PG,PROPLOCAT,,LANDTYPE,YRBUILT1,SALEPRICE,SALETYPE,SALEDATE,ORTHO
+columns = ["property_id", "book", "page", "physical_address", "year_built", "sale_price", "sale_date", "state", "county", "source_url"]
+
+with open("Parcels2021Download.csv", "r") as input_csv:
+    reader = csv.DictReader(input_csv)
+
+    with open("extracted_data.csv", "a", newline="") as output_csv:
+        writer = csv.DictWriter(output_csv, fieldnames=columns)
+        writer.writeheader()
+
+        for row in reader:
+            land_info = {
+                "property_id": row]"PARCEL_ID",
+                "book": row["BK"],
+                "page": row["PG"],
+                "physical_address": " ".join(str(row["PROPLOCAT"]).upper().strip().split()),
+                "year_built": row["YRBUILT1"],
+                "sale_price": row["SALEPRICE"],
+                "sale_date": str(row["SALEDATE"]).replace("+00", ""),
+                "state": "NC",
+                "county": "WILKES",
+                "source_url": "https://data3-wilkescountygis.opendata.arcgis.com/datasets/wilkescountygis::parcels2021download/about"
+
+            }
+
+            if land_info["physical_address"] and land_info["sale_date"] and land_info["sale_price"]:
+                writer.writerow(land_info)
