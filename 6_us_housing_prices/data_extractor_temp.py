b8da51c51dde04195eca484402dbce11150c6684
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-01-24 12:01:57 -0500

    add template data extractor

---
+++ 6_us_housing_prices/data_extractor_temp.py
@@ -0,0 +1,64 @@
+import csv
+from tqdm import tqdm
+from dateutil import parser
+
+#
+columns = ["county", "state", "source_url"]
+with open("TaxParcels.csv", "r") as input_csv:
+    line_count = len([line for line in input_csv.readlines()])
+    input_csv.seek(0)
+    reader = csv.DictReader(input_csv)
+
+    with open("extracted_data.csv", "a", newline="") as output_csv:
+        writer = csv.DictWriter(output_csv, fieldnames=columns)
+        writer.writeheader()
+
+        for row in tqdm(reader, total=line_count):
+            try:
+                land_info = {
+
+                }
+
+                # If address is in separate fields
+                street_list = [str(row["Street#"]).strip(), str(row["Direction"]).strip()]
+
+                # concat the street parts filtering out blank parts
+                land_info["physical_address"] = ' '.join(filter(None, street_list)).upper()
+
+                # Delete if no book
+                # Update field
+                book = row["DEED_BOOK"]
+                page = row["DEED_PAGE"]
+
+                try:
+                    if int(book) != 0 and int(page) != 0:
+                        land_info["book"] = book
+                        land_info["page"] = page
+
+                except ValueError:
+                    continue
+
+                # Delete if no year_built
+                try:
+                    if int(row["YEARBLT"]) != 0 and int(row["YEARBLT"]) <= 2022:
+                        land_info["year_built"] = row["YEARBLT"]
+
+                except ValueError:
+                    continue
+
+                # Delete if no zip5
+                if land_info["zip5"] == "00000" or land_info["zip5"] == "0":
+                    land_info["zip5"] = ""
+
+                # Delete if no unit count
+                if int(row["TOTAL_UNITS"]) != 0:
+                    land_info["num_units"] = row["TOTAL_UNITS"]
+
+
+                year = land_info["sale_date"].split("-")[0]
+
+                if land_info["physical_address"] and land_info["sale_date"] and land_info["sale_price"] != "" and int(year) <= 2022:
+                    writer.writerow(land_info)
+
+            except parser._parser.ParserError:
+                continue
