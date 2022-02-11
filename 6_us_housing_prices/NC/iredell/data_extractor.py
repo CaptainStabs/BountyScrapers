7aa7be7329f50fbc8ec0f1be31f43b3243cf42ae
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-02-11 16:11:54 -0500

    add sale-type to iredell nc

--- 6_us_housing_prices/NC/iredell/data_extractor.py
+++ 6_us_housing_prices/NC/iredell/data_extractor.py
@@ -1,9 +1,16 @@
 import csv
 from tqdm import tqdm
 from dateutil import parser
+from pathlib import Path
+import sys
+
+p = Path(__file__).resolve().parents[2]
+sys.path.insert(1, str(p))
+
+from _sale_type.sale_type import sale_type
 
 # ,PIN,DWBook,DWPage,Sale_Date,Sale_Yr,Sales_Price,Dwelling_Style,,HouseNumber,SDIR,STREET,STYPE,ST_SUFFIX,TownshipLocationDescription,
-columns = ["property_id", "book", "page", "sale_date", "sale_price", "property_type", "physical_address", "city", "county", "state", "source_url"]
+columns = ["property_id", "book", "page", "sale_date", "sale_price", "property_type", "physical_address", "sale_type", "city", "county", "state", "source_url"]
 with open("TaxSQL_Parcels.csv", "r") as input_csv:
     line_count = len([line for line in input_csv.readlines()])
     input_csv.seek(0)
@@ -19,12 +26,18 @@ with open("TaxSQL_Parcels.csv", "r") as input_csv:
                     "property_id": str(row["PIN"]).split(".")[0],
                     "sale_date": str(parser.parse(row["Sale_Date"])),
                     "sale_price": row["Sales_Price"],
-                    "property_type": " ".join(str(row["Dwelling_Style"]).strip().split()),
+                    "property_type": " ".join(str(row["Dwelling_Style"]).strip().split()).upper(),
                     "city": " ".join(str(row["TownshipLocationDescription"]).upper().strip().split()),
-                    "county": "Iredell",
+                    "county": "IREDELL",
                     "state": "NC",
                     "source_url": "https://iredell-datadown-iredell.opendata.arcgis.com/datasets/3433d6293c594e51ac2a6466103c808f_0"
                 }
+
+                try:
+                    land_info["sale_type"] = sale_type[row["DocType"].upper().replace("_", "").strip()]
+
+                except KeyError:
+                    land_info["sale_type"] = str(row["DocType"]).upper().strip()
 
                 # If address is in separate fields
                 street_list = [str(row["HouseNumber"]).strip(), str(row["SDIR"]).strip(), str(row["STREET"]).strip(), str(row["STYPE"]).strip(), str(row["ST_SUFFIX"]).strip()]
