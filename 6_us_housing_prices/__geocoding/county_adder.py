bbffac4f16d32b98c447f82222f487e616ba0334
CaptainStabs <comportfailure@gmail.com>
2022-02-12 16:26:24 -0500

    check +- 1 for each keyerror on zip5

--- 6_us_housing_prices/__geocoding/county_adder.py
+++ 6_us_housing_prices/__geocoding/county_adder.py
@@ -1,70 +1,100 @@
 import yaml
 import csv
 from tqdm import tqdm
-
+# import heartrate; heartrate.trace(browser=True)
 
 columns = ["state", "zip5", "physical_address", "city", "county", "property_id", "sale_date", "property_type", "sale_price", "seller_name", "buyer_name", "num_units", "year_built", "source_url", "book", "page", "sale_type"]
 with open("geo-data.yaml", "r") as f:
     zip_cty_cnty = yaml.safe_load(f)
+    # print(zip_cty_cnty[31419])
 
-with open("F:\\us-housing-prices-2\\null_counties.csv", "r") as input_csv:
-    line_count = len([line for line in input_csv.readlines()])
+with open("F:\\us-housing-prices-2\\t.csv", "r") as input_csv:
+    line_count = 3489716 #len([line for line in input_csv.readlines()])
     input_csv.seek(0)
     reader = csv.DictReader(input_csv)
 
     with open("F:\\us-housing-prices-2\\added_counties.csv", "a") as output_csv:
         writer = csv.DictWriter(output_csv, fieldnames=columns)
         writer.writeheader()
-        
+
         for row in tqdm(reader, total=line_count):
+            success = False
+            zip_error = False
             if row["zip5"] and row["city"]:
                 try:
-                    county = zip_cty_cnty["zip5"]["county"]
-                    land_info = {
-                        "state": row["state"],
-                        "zip5": row["zip5"],
-                        "physical_address": row["physical_address"],
-                        "city": row["city"],
-                        "county": str(county).upper().strip(),
-                        "property_id": row["property_id"],
-                        "sale_date": row["sale_date"],
-                        "sale_price": row["sale_price"],
-                        "seller_name": row["seller_name"],
-                        "buyer_name": row["buyer_name"],
-                        "num_units": row["num_units"],
-                        "year_built": row["year_built"],
-                        "source_url": row["source_url"],
-                        "book": row["book"],
-                        "page": row["page"],
-                        "sale_type": row["sale_type"]
-                    }
-
+                    county = zip_cty_cnty[row["zip5"]]["county"]
                 except KeyError:
-                    pass
+                    try:
+                        print("\n1:", int(row["zip5"])+1)
+                        county = zip_cty_cnty[str(int(row["zip5"])+1)]["county"]
+                    except KeyError:
+                        try:
+                            print("\n2:", int(row["zip5"])-1)
+                            county = zip_cty_cnty[str(int(row["zip5"])-1)]["county"]
+                        except KeyError:
+                            zip_error = True
+                            print(row["zip5"])
+
+                    if not zip_error:
+                        land_info = {
+                            "state": row["state"],
+                            "zip5": row["zip5"],
+                            "physical_address": row["physical_address"],
+                            "city": row["city"],
+                            "county": str(county).upper().strip(),
+                            "property_id": row["property_id"],
+                            "sale_date": row["sale_date"],
+                            "sale_price": row["sale_price"],
+                            "seller_name": row["seller_name"],
+                            "buyer_name": row["buyer_name"],
+                            "num_units": row["num_units"],
+                            "year_built": row["year_built"],
+                            "source_url": row["source_url"],
+                            "book": row["book"],
+                            "page": row["page"],
+                            "sale_type": row["sale_type"]
+                        }
+                        success = True
+
+
             elif row["zip5"] and not row["city"]:
-                county = zip_cty_cnty["zip5"]["county"]
-                city = zip_cty_cnty["zip5"]["city"]
-                land_info = {
-                    "state": row["state"],
-                    "zip5": row["zip5"],
-                    "physical_address": row["physical_address"],
-                    "city": str(city).upper().strip(),
-                    "county": str(county).upper().strip(),
-                    "property_id": row["property_id"],
-                    "sale_date": row["sale_date"],
-                    "sale_price": row["sale_price"],
-                    "seller_name": row["seller_name"],
-                    "buyer_name": row["buyer_name"],
-                    "num_units": row["num_units"],
-                    "year_built": row["year_built"],
-                    "source_url": row["source_url"],
-                    "book": row["book"],
-                    "page": row["page"],
-                    "sale_type": row["sale_type"]
-                }
-
                 try:
-                    writer.writerow(land_info)
+                    county = zip_cty_cnty[row["zip5"]]["county"]
+                    city = zip_cty_cnty[row["zip5"]]["city"]
+                except KeyError:
+                    try:
+                        county = zip_cty_cnty[str(int(row["zip5"])+1)]["county"]
+                        city = zip_cty_cnty[str(int(row["zip5"])+1)]["city"]
+                    except KeyError:
+                        try:
+                            county = zip_cty_cnty[str(int(row["zip5"])-1)]["county"]
+                            city = zip_cty_cnty[str(int(row["zip5"])-1)]["city"]
+                        except KeyError:
+                            print(row["zip5"])
+                            zip_error = True
 
-                except Exception as e:
-                    print("E")
+                    if not zip_error:
+                        land_info = {
+                            "state": row["state"],
+                            "zip5": row["zip5"],
+                            "physical_address": row["physical_address"],
+                            "city": str(city).upper().strip(),
+                            "county": str(county).upper().strip(),
+                            "property_id": row["property_id"],
+                            "sale_date": row["sale_date"],
+                            "sale_price": row["sale_price"],
+                            "seller_name": row["seller_name"],
+                            "buyer_name": row["buyer_name"],
+                            "num_units": row["num_units"],
+                            "year_built": row["year_built"],
+                            "source_url": row["source_url"],
+                            "book": row["book"],
+                            "page": row["page"],
+                            "sale_type": row["sale_type"]
+                        }
+                        success = True
+
+            if success:
+                writer.writerow(land_info)
+            if zip_error:
+                break
