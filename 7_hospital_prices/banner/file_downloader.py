2b515c2ea1c961dbf6d21bb57eb8a1bed7b11637
CaptainStabs <comportfailure@gmail.com>
2022-02-26 14:38:39 -0500

    filename isn't needed

--- 7_hospital_prices/banner/file_downloader.py
+++ 7_hospital_prices/banner/file_downloader.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor, as_complete
 
 
-def download_file(url, filename):
+def download_file(url):
     headers = {'Cookie': 'ApplicationGatewayAffinity=28d3f836d72c113f3c64ba238c7a7bbf4d64bf76b299fd91f5fc559d5a278bd6; ApplicationGatewayAffinityCORS=28d3f836d72c113f3c64ba238c7a7bbf4d64bf76b299fd91f5fc559d5a278bd6'}
     hash = url.split('/')[-1].strip('"')
     url = f"https://apim.services.craneware.com/api-pricing-transparency/api/public/{hash}/metadata/cdmFile"
@@ -19,6 +19,6 @@ if __name__ == "__main__":
    with open("banner_urls.csv", "r") as f:
        with ThreadPoolExecutor(max_workers=10) as executor:
         for row in tqdm(f):
-            filename, url = row.strip().split(",")
-            threads.append(executor.submit(download_file(url, filename.strip('"').replace("'", ""))))
+            url = row.strip().split(",")[1]
+            threads.append(executor.submit(download_file, url))
 
