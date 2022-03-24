119a0286fad5faa9499a858ee6360a0a6d7081f0
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-03-24 16:07:08 -0400

    update hospitals

---
+++ 7_hospital_prices/ardent/file_splitter.py
@@ -0,0 +1,20 @@
+from tqdm import tqdm
+
+chunk_size = 4595338
+def write_chunk(part, lines):
+    with open('./split_files/split_file_'+ str(part) +'.csv', 'w') as f_out:
+        f_out.write(header)
+        f_out.writelines(lines)
+with open("cleaned_data.csv", "r") as f:
+    count = 0
+    header = f.readline()
+    lines = []
+    for line in tqdm(f, total=13786014):
+        count += 1
+        lines.append(line)
+        if count % chunk_size == 0:
+            write_chunk(count // chunk_size, lines)
+            lines = []
+    # write remainder
+    if len(lines) > 0:
+        write_chunk((count // chunk_size) + 1, lines)
