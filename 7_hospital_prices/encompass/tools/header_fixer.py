446d346b73d59c69ec2032f8780b0405b5de2ae3
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-03-30 14:38:26 -0400

    fix header fixer

--- 7_hospital_prices/encompass/tools/header_fixer.py
+++ 7_hospital_prices/encompass/tools/header_fixer.py
@@ -6,12 +6,11 @@ directory = "./input_files/"
 output_dir = "./output_files/"
 def header_fixer(directory, output_dir, file, lines_to_header, fix_header=False):
         # print(os.path.join(directory, file))
-        with open(os.path.join(directory, file), "r", encoding="utf-8") as f:
-            with open(os.path.join(output_dir, file), "a", encoding="utf-8") as out_f:
+        with open(os.path.join(directory, file), "r") as f:
+            with open(os.path.join(output_dir, file), "a") as out_f:
                 for i, line in enumerate(f):
-                    if i < int(lines_to_header):
-                        continue
-
+                    if i < int(lines_to_header): continue
+                    if ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,," in line: continue
 
                     out_f.write(line.strip('"'))
 
@@ -19,7 +18,8 @@ if __name__ == "__main__":
     threads = []
     with ThreadPoolExecutor(max_workers=5) as executor:
         for file in tqdm(os.listdir(directory)):
-            threads.append(executor.submit(header_fixer, directory, output_dir, file, 5, 4))
+            # threads.append(executor.submit(header_fixer, directory, output_dir, file, 5, 4))
+            header_fixer(directory, output_dir, file, 5, 4)
 
-        for task in tqdm(as_completed(threads)):
-            a = "A"
+        # for task in tqdm(as_completed(threads)):
+        #     a = "A"
