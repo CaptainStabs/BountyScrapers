02fe5e14d7874b9e7b965456f2055721a0eb772f
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-04-03 14:13:36 -0400

    update mulit

--- 7_hospital_prices/HCA/multi_file_extractor.py
+++ 7_hospital_prices/HCA/multi_file_extractor.py
@@ -32,7 +32,7 @@ def parse_str(num):
     raise ValueError('num is not a number. Got {}.'.format(num)) # optional
 
 def parse_row(file, writer, columns):
-    rep = {"CPT/HCPC": "", "ICD 9/10": "", "MS-DRG": "", " or ": ""}
+    rep = {"CPT/HCPC": "", "ICD 9/10": "", "MS-DRG": "", " or ": "", ";", ","}
     rep = dict((re.escape(k), v) for k, v in rep.items())
     pattern = re.compile("|".join(rep.keys()))
 
@@ -82,26 +82,40 @@ def parse_row(file, writer, columns):
                                 end_pend = ""
 
                                 x, y = c.split("-")
-                                if isalpha(x[0].strip()) and isalpha(y[0].strip()):
+                                if x[0].strip().isalpha() and y[0].strip().isalpha():
                                     if x[0] == y[0]:
                                         prepend = x[0]
+                                        x, y = x[1:], y[1:]
                                     else:
                                         print("AAA")
                                         print(x[0], y[0])
 
-                                if isalpha(x[-1].strip().strip(";")) and isalpha(y[-1].strip().strip(";")):
+                                if x[-1].strip().strip(";").isalpha() and y[-1].strip().strip(";").isalpha():
                                     if x[-1] == y[-1]:
                                         end_pend = x[-1]
+                                        x, y = x[:-1], y[:-1]
                                     else:
                                         print("AAA")
                                         print(x[-1], y[-1])
 
-                                for codes in np.arange(parse_str(x),parse_str(y)):
-                                    if is_rev:
-                                        price_info["internal_revenue_code"] = str(codes)
-                                        price_info["code"] = "NONE"
-                                    else:
-                                        price_info["code"] = str(codes).zfill(3).strip(";")
+                                if "." in x and "." in y:
+                                    isfloat = True
+                                else:
+                                    isfloat = False
+
+                                # Floats are only ISD codes
+                                if isfloat:
+                                    step = int(y.split(".")[-1].strip()) - int(x.split(".")[-1].strip())
+                                    for codes in np.linspace(parse_str(x),parse_str(y), num=step):
+                                        if is_rev:
+                                            price_info["internal_revenue_code"] = str(codes)
+                                            price_info["code"] = "NONE"
+                                            # print(str(codes))
+                                        else:
+                                            # price_info["code"] = "{:.2f}".format(codes)
+                                            # print("{:.2f}".format(codes))
+                                            price_info["code"] = "".join([prepend, "{:.2f}".format(codes), end_pend]))
+
                                     write_data(price_info, writer, file, row)
                             else:
                                 if not str(c).strip() or str(c).strip() == "N/A" or str(c) == "	":
@@ -119,7 +133,8 @@ def parse_row(file, writer, columns):
                 print(row)
                 tb.print_exc()
                 pass
-
+def d_split(c, price_info, writer, file, row):
+    
 if __name__ == "__main__":
     threads = []
     # "Charge # (Px Code)",Procedure Name,Procedure Code (CPT / HCPCS),Default Modifier,Gross Charge,Discounted Cash Charge,Hospital Inpatient / Outpatient / Both(Px Code)",Procedure Name,Procedure Code (CPT / HCPCS),Default Modifier,Gross Charge,Discounted Cash Charge,Hospital Inpatient / Outpatient / Both
