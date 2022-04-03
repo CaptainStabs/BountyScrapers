c9d539ad64d1d4e45fead0e723d0d566f4de79ed
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-04-03 14:36:06 -0400

    update HCA

--- 7_hospital_prices/HCA/multi_file_extractor.py
+++ 7_hospital_prices/HCA/multi_file_extractor.py
@@ -32,7 +32,7 @@ def parse_str(num):
     raise ValueError('num is not a number. Got {}.'.format(num)) # optional
 
 def parse_row(file, writer, columns):
-    rep = {"CPT/HCPC": "", "ICD 9/10": "", "MS-DRG": "", " or ": "", ";", ","}
+    rep = {"CPT/HCPC": "", "ICD 9/10": "", "MS-DRG": "", " or ": "", ";": ","}
     rep = dict((re.escape(k), v) for k, v in rep.items())
     pattern = re.compile("|".join(rep.keys()))
 
@@ -70,77 +70,30 @@ def parse_row(file, writer, columns):
                     write_data(price_info, writer, file, row)
                 else:
 
-                    if "REV " in code.upper():
+                    if "REV " in code.upper() or "REV" in code.upper():
                         is_rev = True
                         code = code.replace("REV", "")
                     else: is_rev = False
 
                     if "," in code:
                         for c in code.split(",").strip("-"):
-                            if c[0] = "0" and len(c) = 4:
-                                is_rev = True
                             if "-" in c:
-                                prepend = ""
-                                end_pend = ""
-
-                                x, y = c.split("-")
-                                if x[0].strip().isalpha() and y[0].strip().isalpha():
-                                    if x[0] == y[0]:
-                                        prepend = x[0]
-                                        x, y = x[1:], y[1:]
-                                    else:
-                                        print("AAA")
-                                        print(x[0], y[0])
-
-                                if x[-1].strip().strip(";").isalpha() and y[-1].strip().strip(";").isalpha():
-                                    if x[-1] == y[-1]:
-                                        end_pend = x[-1]
-                                        x, y = x[:-1], y[:-1]
-                                    else:
-                                        print("AAA")
-                                        print(x[-1], y[-1])
-
-                                if "." in x and "." in y:
-                                    isfloat = True
-                                else:
-                                    isfloat = False
-
-                                # Floats are only ISD codes
-                                if isfloat:
-                                    step = int(y.split(".")[-1].strip()) - int(x.split(".")[-1].strip())
-                                    for codes in np.linspace(parse_str(x),parse_str(y), num=step):
-                                        if is_rev:
-                                            price_info["internal_revenue_code"] = str(codes)
-                                            price_info["code"] = "NONE"
-                                            # print(str(codes))
-                                        else:
-                                            # price_info["code"] = "{:.2f}".format(codes)
-                                            # print("{:.2f}".format(codes))
-                                            price_info["code"] = "".join([prepend, "{:.2f}".format(codes), end_pend]))
-
-                                    write_data(price_info, writer, file, row)
-                                else:
-                                    for codes in np.arange(parse_str(x),parse_str(y)):
-                                        if is_rev:
-                                            price_info["internal_revenue_code"] = str(codes)
-                                            price_info["code"] = "NONE"
-                                            # print(str(codes))
-                                        else:
-                                            price_info["code"] = str(codes).zfill(3).strip(";")
-                                            print(str(codes).zfill(3).strip(";"))
-
-                                        if prepend or end_pend:
-                                            print("".join([prepend, str(codes), end_pend]))
+                                d_split(c, price_info, writer, file, row)
                             else:
+                                is_rev = rev_checker(c)
                                 if not str(c).strip() or str(c).strip() == "N/A" or str(c) == "	":
                                     price_info["code"] = "NONE"
                                 else:
-                                    if
                                     if is_rev:
                                         price_info["internal_revenue_code"] = c.strip().strip(";")
+                                        price_info["code"] = "NONE"
+
                                     else:
                                         price_info["code"] = c.strip().strip(";")
-                                write_data(price_info, writer, file, row)
+
+                                    write_data(price_info, writer, file, row)
+                    elif "-" in code.strip("-"):
+                        d_split(code, price_info, writer, file, row)
                     else:
                         if code[0] = "0" and len(code) == 4:
                             price_info["code"] = "NONE"
@@ -151,12 +104,74 @@ def parse_row(file, writer, columns):
                 print(row)
                 tb.print_exc()
                 pass
-def d_split(c, price_info, writer, file, row):
 
+def d_split(c, price_info, writer, file, row):
+    is_rev = rev_checker(c)
+    prepend = ""
+    end_pend = ""
+
+    # Split by - to get min and max
+    x, y = c.split("-")
+    # Check if the first character is a letter, so that I can add it back in later
+    if x[0].strip().isalpha() and y[0].strip().isalpha():
+        if x[0] == y[0]:
+            prepend = x[0]
+            # Remove the first character to prevent errors
+            x, y = x[1:], y[1:]
+        else:
+            print("AAA")
+            print(x[0], y[0])
+    # Check last if last character is a letter, so that I can add it back in later
+    if x[-1].strip().strip(";").isalpha() and y[-1].strip().strip(";").isalpha():
+        if x[-1] == y[-1]:
+            end_pend = x[-1]
+            x, y = x[:-1], y[:-1]
+        else:
+            print("AAA")
+            print(x[-1], y[-1])
+
+    # Naive way to check if x is a float
+    if "." in x and "." in y:
+        isfloat = True
+    else:
+        isfloat = False
+
+    # Floats are only ICD 9/10 codes afaict
+    if isfloat:
+        step = int(y.split(".")[-1].strip()) - int(x.split(".")[-1].strip())
+        for codes in np.linspace(parse_str(x),parse_str(y), num=step):
+            if is_rev:
+                # Figured I might as well leave this in just in case I'm wrong
+                price_info["internal_revenue_code"] = str(codes)
+                price_info["code"] = "NONE"
+            else:
+                price_info["code"] = "".join([prepend, "{:.2f}".format(codes), end_pend]))
+
+            write_data(price_info, writer, file, row)
+    else:
+        for codes in np.arange(parse_str(x),parse_str(y) + 1):
+            if is_rev:
+                price_info["internal_revenue_code"] = str(codes)
+                price_info["code"] = "NONE"
+                # print(str(codes))
+            else:
+                price_info["code"] = str(codes).zfill(3).strip(";")
+                print(str(codes).zfill(3).strip(";"))
+
+            if prepend or end_pend:
+                print("".join([prepend, str(codes), end_pend]))
+
+def rev_checker(c):
+    if c[0] == "0" and len(c) == 4:
+        return True
+    else: return False
 if __name__ == "__main__":
     threads = []
     # "Charge # (Px Code)",Procedure Name,Procedure Code (CPT / HCPCS),Default Modifier,Gross Charge,Discounted Cash Charge,Hospital Inpatient / Outpatient / Both(Px Code)",Procedure Name,Procedure Code (CPT / HCPCS),Default Modifier,Gross Charge,Discounted Cash Charge,Hospital Inpatient / Outpatient / Both
     columns = ["cms_certification_num", "code","description", "payer", "price", "inpatient_outpatient", "code_disambiguator", "internal_revenue_code"]
+    if os.path.exists("extracted_data.csv"):
+        os.remove("extracted_data.csv")
+
     with open(f"extracted_data.csv", "a", newline="") as output_csv:
         writer = csv.DictWriter(output_csv, fieldnames=columns)
         writer.writeheader()
