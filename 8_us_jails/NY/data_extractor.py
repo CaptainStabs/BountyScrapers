1e34fc4485332baba291a6e6241774ab92197717
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-04-20 12:28:20 -0400

    finish update to polars

--- 8_us_jails/NY/data_extractor.py
+++ 8_us_jails/NY/data_extractor.py
@@ -33,25 +33,28 @@ with open("extracted_data.csv", "a") as output_csv:
         #
         #             }
 
-        df = pd.read_csv(f"./files/{file}")
+        df = pl.read_csv(f"./files/{file}")
         print(df)
         # Drop last column (date range)
-        df = df.drop('3/2021 vs\r\n3/2022')
-        print(df)
-        break
-        # Rename columns
-        # df.columns=["jail", "type", "2021-04-01", "2021-05-01", "2021-06-01", "2021-07-01", "2021-08-01", "2021-09-01", "2021-10-01", "2021-11-01", "2021-12-01", "2022-01-01", "2022-02-01", "2022-03-01", "2022-04-01"]
-        # df.drop("jail", axis=1, inplace=True)
-        # print(df)
-        # df = df.transpose()
-        # df = df.drop(df.columns[[0,2]], axis=1)
+        df = df.drop(['jail', 'delete'])
+        # Remove old header
+        df = df[1:]
 
-        # df.columns=["total_off_site", "total", "convicted_or_sentenced", "civil_offense", "federal_offense", "technical_parole_violators", "state_readies", "detained_or_awaiting_trial"]
-        # # Remove old header row
-        # df = df.iloc[1:, :]
-        # # print(df)
-        # df["detained_or_awaiting_trial"] = df[["detained_or_awaiting_trial", "state_readies"]].sum(axis=1)
-        # df.drop("state_readies", axis=1, inplace=True)
-        # # df.columns=["snapshot_date", "total_off_site", "total", "convicted_or_sentenced", "detained_or_awaiting_trial", "other_offense"]
-        # df["id"] = file
+        df = df.transpose(include_header=True)
+        df = df.drop(["column_0", "column_2"])
+        print(df)
+        df.columns = ["snapshot_date", "total_off_site", "total", "convicted_or_sentenced", "civil_offense", "federal_offense", "technical_parole_violators", "state_readies", "detained_or_awaiting_trial"]
+        df = df[1:]
+        print(df)
+        df2 = df.to_pandas()
+        con_col = ["state_readies", "detained_or_awaiting_trial"]
+        # df2[con_col] = pd.to_numeric(df2[con_col].stack(), downcast="integer", errors='coerce').unstack()
+        df2["state_readies"] = pd.to_numeric(df2["state_readies"], downcast="integer")
+        df2["detained_or_awaiting_trial"] = pd.to_numeric(df2["detained_or_awaiting_trial"], downcast="integer")
+
+        df2["detained_or_awaiting_trial"] = df2[["detained_or_awaiting_trial", "state_readies"]].sum(axis=1)
+        df = pl.from_pandas(df2)
+        df = df.drop("state_readies")
+        df["id"] = file
+    
         # df.to_csv("extracted_data", mode="a")
