abb85486a4bbc1c18f0787268b5f92cdbc14bae1
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-04-20 11:03:22 -0400

    switch to polars

--- 8_us_jails/NY/data_extractor.py
+++ 8_us_jails/NY/data_extractor.py
@@ -3,9 +3,10 @@ from tqdm import tqdm
 import traceback as tb
 import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import polars as pl
 import pandas as pd
 # import heartrate; heartrate.trace(browser=True, daemon=True)
-
+# pl.set_option('display.max_columns', 8)
 columns = ["snapshot_date", "total_off_site", "total", "convicted_or_sentenced", "detained_or_awaiting_trial", "other_offense", "id"]
 snap_dates = ["2021-04-01", "2021-05-01", "2021-06-01", "2021-07-01", "2021-08-01", "2021-09-01", "2021-10-01", "2021-11-01", "2021-12-01", "2022-02-01", "2022-03-01", "2022-04-01"]
 in_col = {
@@ -33,24 +34,24 @@ with open("extracted_data.csv", "a") as output_csv:
         #             }
 
         df = pd.read_csv(f"./files/{file}")
-        df = df.iloc[: , :-1]
-        # print(df)
-        df.columns=["jail", "type", "2021-04-01", "2021-05-01", "2021-06-01", "2021-07-01", "2021-08-01", "2021-09-01", "2021-10-01", "2021-11-01", "2021-12-01", "2022-01-01", "2022-02-01", "2022-03-01", "2022-04-01"]
-        df.drop("jail", 1, inplace=True)
-        df = df.transpose()
-        df = df.drop(df.columns[[0,2]], axis=1)
-        # print(df)
-        df.columns=["total_off_site", "total", "convicted_or_sentenced", "civil", "federal", "technical_parole_violators", "state_readies", "detained_or_awaiting_trial"]
-        df = df.iloc[1:, :]
-        # print(df)
-        df["other_offense"] = df[["civil", "federal", "state_readies"]].sum(axis=1)
-        df = df.drop(df.columns[[3,4,6]], axis=1)
-        df.columns=["snapshot_date", "total_off_site", "total", "convicted_or_sentenced", "detained_or_awaiting_trial", "other_offense"]
-        df["id"] = file
-        print(df['snapshot_date'].to_csv(index=False))
-
-
-        # o_df = pd.DataFrame(columns=columns)
-        # o_df["id"] = file
-        # print(o_df)
+        print(df)
+        # Drop last column (date range)
+        df = df.drop('3/2021 vs\r\n3/2022')
+        print(df)
         break
+        # Rename columns
+        # df.columns=["jail", "type", "2021-04-01", "2021-05-01", "2021-06-01", "2021-07-01", "2021-08-01", "2021-09-01", "2021-10-01", "2021-11-01", "2021-12-01", "2022-01-01", "2022-02-01", "2022-03-01", "2022-04-01"]
+        # df.drop("jail", axis=1, inplace=True)
+        # print(df)
+        # df = df.transpose()
+        # df = df.drop(df.columns[[0,2]], axis=1)
+
+        # df.columns=["total_off_site", "total", "convicted_or_sentenced", "civil_offense", "federal_offense", "technical_parole_violators", "state_readies", "detained_or_awaiting_trial"]
+        # # Remove old header row
+        # df = df.iloc[1:, :]
+        # # print(df)
+        # df["detained_or_awaiting_trial"] = df[["detained_or_awaiting_trial", "state_readies"]].sum(axis=1)
+        # df.drop("state_readies", axis=1, inplace=True)
+        # # df.columns=["snapshot_date", "total_off_site", "total", "convicted_or_sentenced", "detained_or_awaiting_trial", "other_offense"]
+        # df["id"] = file
+        # df.to_csv("extracted_data", mode="a")
