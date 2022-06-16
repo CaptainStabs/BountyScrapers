4c4c9927ad24b656d9a8f903d4038ae79b5c6922
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-06-16 15:44:26 -0400

    finish POC

--- 9_arts/auckland/human_scraper.py
+++ 9_arts/auckland/human_scraper.py
@@ -3,6 +3,7 @@ import json
 import csv
 import os
 import functools
+import html
 
 url = "https://api.aucklandmuseum.com/id/humanhistory/object/100299"
 headers = {
@@ -20,26 +21,26 @@ headers = {
 }
 
 @functools.lru_cache(maxsize=None)
-def url_get(url)
+def url_get(url):
     r = requests.get(url, headers=headers).json()
     return r
 
 @functools.lru_cache(maxsize=None)
 def get_category(url):
-    r = requests.get(url, headers=headers).json()
+    r = url_get(url)
     category = r["am:displayValue"][0]["value"]
     return category
 
 
 def get_title(url):
-    r = requests.get(url, headers=headers).json()
+    r = url_get(url)
     return r["rdf:value"][0]["value"]
 
 def get_material_url(url_list):
     mat_list = []
 
     for url in url_list:
-        r = requests.get(url["value"], headers=headers).json()
+        r = url_get(url["value"])
         mats = r["ecrm:P2_has_type"]
         for uri in mats:
             mat_list.append(get_material(uri["value"]))
@@ -48,28 +49,36 @@ def get_material_url(url_list):
 @functools.lru_cache(maxsize=None)
 def get_material(url):
     mat_list = []
-    r = requests.get(url, headers=headers).json()
+    r = url_get(url)
     for mats in r["am:displayValue"]:
         mat_list.append(mats["value"])
     return "|".join(mat_list)
 
 def get_location_url(url):
-    r = requests.get(url, headers=headers).json()
+    r = url_get(url)
     url = r["ecrm:P7_took_place_at"][0]["value"]
     loc = get_location(url)
     return loc
 
 @functools.lru_cache(maxsize=None)
 def get_location(url):
-    r = requests.get(url, headers=headers).json()
+    r = url_get(url)
     return r["am:displayValue"][0]["value"]
 
 def get_date(url):
-    r = requests.get(url, headers=headers).json()
+    r = url_get(url)
+    d = url_get(r["ecrm:P10_falls_within"][0]["value"])
+    return d["rdf:type"][0]["value"].strip("ecrm:").replace("_", " ")
+
+def accession_year(an):
+    a = an.split(".")[0]
+    if len(a) == 4 and a.isdigit():
+        return a
+    else:
+        return None
 
 
-
-columns = []
+columns = ['object_number', 'institution_name', 'institution_city', 'institution_state', 'institution_country', 'institution_latitude', 'institution_longitude', 'category', 'title', 'description', 'materials', 'from_location', 'culture', 'date_description', 'accession_number', 'accession_year', 'credit_line', 'source_1']
 
 with open("extracted_data.csv", "a") as output_file:
     writer = csv.DictWriter(output_file, fieldnames=columns)
@@ -94,12 +103,18 @@ with open("extracted_data.csv", "a") as output_file:
         "institution_latitude": 33.9211533,
         "institution_longitude": -84.3692509,
         "category": get_category(r["am:classification"][0]["value"]),
-        "title": get_title(r["am:nameTitle"][0]["value"]),
+        "title": r["dc:title"][0]["value"],
         "description": r["dc:description"][0]["value"],
         "materials": get_material_url(r["ecrm:P45_consists_of"]),
         "from_location": get_location_url(r["ecrm:P108i_was_produced_by"][0]["value"]),
         "culture": r["am:culturalOrigin"][0]["value"],
-        "date_description"
+        "date_description": get_date(r["ecrm:P108i_was_produced_by"][0]["value"]),
+        "accession_number": r["am:accessionNumber"][0]["value"],
+        "accession_year": accession_year(r["am:accessionNumber"][0]["value"]),
+        "credit_line": str(r["am:creditLine"][0]["value"]),
+        "source_1": url
     }
+
+    print(list(data.keys()))
 
     print(json.dumps(data, indent=4))
