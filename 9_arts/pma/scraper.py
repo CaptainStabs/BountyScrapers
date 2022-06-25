104d0f038540c748c73092c7207786b762180a12
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-06-25 18:31:58 -0400

    finish scraper before testing

--- 9_arts/pma/scraper.py
+++ 9_arts/pma/scraper.py
@@ -1,10 +1,21 @@
-import requests
-import time
-import json
 import csv
+import json
 import os
+import time
+
+import pandas as pd
+import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from tqdm import tqdm
+
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+import sys
+from pathlib import Path
+
+p = Path(__file__).resolve().parents[1]
+sys.path.insert(1, str(p))
+from _common import get_last_id
 
 filename =  "extracted_data.csv"
 
@@ -28,17 +39,35 @@ columns = [
     "current_location",
     "image_url",
     "source_1",
+    "drop_me"
     ]
 
-with open(filename, "a", encoding='utf-8') as output_file:
+with open(filename, "a", encoding='utf-8', newline='') as output_file:
     writer = csv.DictWriter(output_file, fieldnames=columns)
+
+    if os.path.exists(filename) and os.stat(filename).st_size > 283:
+        start_id = get_last_id(filename)
+    else:
+        start_id = 0
 
     if os.stat(filename).st_size == 0:
         writer.writeheader()
 
-    for i in range(0, 34901):
+    for i in tqdm(range(start_id, 298920)):
         url = f"https://hackathon.philamuseum.org/api/v0/collection/object?query={i}&api_token=jnFV09XMKDJ0yVYrcQd3si72VFN4EUeM15B479G6RkMfiu4BstY2GuaR19kI"
         r = requests.get(url, verify=False)
+
+        ratelimit_remaining = int(r.headers['X-RateLimit-Remaining'])
+        if ratelimit_remaining < 40:
+            print(ratelimit_remaining)
+
+        if ratelimit_remaining == 2:
+            print("Sleeping for 60")
+            time.sleep(60)
+            r = requests.get(url, verify=False)
+            if r.headers["x-RateLimit-Remaining"] == 1: # don't use var here because we need to check current r headers
+                print("Has not reset, exiting.")
+                os.exit(1)
 
         jd = r.json()
 
@@ -57,12 +86,22 @@ with open(filename, "a", encoding='utf-8') as output_file:
             "from_location": jd["Geography"].replace("\\u", "\\u"),
             "maker_full_name": "|".join([x["Artist"] for x in jd["Artists"]]),
             "maker_role": "|".join([x["Role"] for x in jd["Artists"]]),
-            "maker_birth_year": "|".join([x["Artist_Info"].split(",")[-1].split("-")[0].strip() for x in jd["Artists"]]),
-            "maker_death_year": "|".join([x["Artist_Info"].split(",")[-1].split("-")[-1].strip() for x in jd["Artists"]]),
             "current_location": jd["Location"],
             "image_url": jd["Image"],
-            "source_1": url
+            "source_1": url,
+            "drop_me": i
         }
-        
+
+        try:
+            data["maker_birth_year"] = "|".join([x["Artist_Info"].split(",")[-1].split("-")[0].strip() for x in jd["Artists"]])
+            data["maker_death_year"] = "|".join([x["Artist_Info"].split(",")[-1].split("-")[-1].strip() for x in jd["Artists"]])
+        except AttributeError:
+            print("Artist info is null")
+            print(jd["Artists"])
+
+        if len(jd["Artists"]) == 1:
+            if "unknown" in jd["Artists"][0]["Artist"]:
+                data["maker_role"] = ""
+                print("Nulling maker_role for", jd["Artists"])
+
         writer.writerow(data)
-        break
