42144884072afd98e78bfe7050d8e112f4dd547e
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-06-25 20:08:55 -0400

    add sleep counter, api key changes, fixes

--- 9_arts/pma/scraper.py
+++ 9_arts/pma/scraper.py
@@ -17,7 +17,19 @@ p = Path(__file__).resolve().parents[1]
 sys.path.insert(1, str(p))
 from _common import get_last_id
 
+def sleep_counter(seconds):
+    for remaining in range(seconds, 0, -1):
+        sys.stdout.write("\r")
+        sys.stdout.write("Too many requests, waiting for {:2d} seconds.".format(remaining))
+        sys.stdout.flush()
+        time.sleep(1)
+
+def url_get(id, key, s):
+    url = f"https://hackathon.philamuseum.org/api/v0/collection/object?query={id}&api_token={cur_key}"
+    return s.get(url, verify=False)
+
 filename =  "extracted_data.csv"
+
 
 columns = [
     "object_number",
@@ -43,6 +55,10 @@ columns = [
     ]
 
 with open(filename, "a", encoding='utf-8', newline='') as output_file:
+    key_list = ["d3INVaCoZxa1PbuLmm3RhghHcdSJQzF0XHhXqxVWQez0VCBREn8cgGtajOK5", "jnFV09XMKDJ0yVYrcQd3si72VFN4EUeM15B479G6RkMfiu4BstY2GuaR19kI"]
+    keys = iter(key_list)
+
+    cur_key = key_list[0]
     writer = csv.DictWriter(output_file, fieldnames=columns)
 
     if os.path.exists(filename) and os.stat(filename).st_size > 283:
@@ -53,55 +69,72 @@ with open(filename, "a", encoding='utf-8', newline='') as output_file:
     if os.stat(filename).st_size == 0:
         writer.writeheader()
 
+    s = requests.Session()
     for i in tqdm(range(start_id, 298920)):
-        url = f"https://hackathon.philamuseum.org/api/v0/collection/object?query={i}&api_token=jnFV09XMKDJ0yVYrcQd3si72VFN4EUeM15B479G6RkMfiu4BstY2GuaR19kI"
-        r = requests.get(url, verify=False)
-
+        url = f"https://hackathon.philamuseum.org/api/v0/collection/object?query={i}"
+        r = url_get(i, cur_key, s)
         ratelimit_remaining = int(r.headers['X-RateLimit-Remaining'])
+        # print(ratelimit_remaining)
         if ratelimit_remaining < 40:
-            print(ratelimit_remaining)
+            print("\n", ratelimit_remaining)
 
-        if ratelimit_remaining == 2:
+        if ratelimit_remaining == 10:
             print("Sleeping for 60")
-            time.sleep(60)
-            r = requests.get(url, verify=False)
+            # sleep_counter(120)
+            r = url_get(i, next(keys), s)
+            print("ratelimit remaining under 10", r.headers["X-RateLimit-Remaining"])
             if r.headers["x-RateLimit-Remaining"] == 1: # don't use var here because we need to check current r headers
                 print("Has not reset, exiting.")
                 os.exit(1)
 
-        jd = r.json()
+        if r.status_code == 429:
+            retry_after = int(r.headers["retry-after"]) + 1
+            sleep_counter(retry_after)
+            cur_key = next(keys)
+            r = url_get(i, cur_key, s)
+            if not r.headers["x-RateLimit-Remaining"]: # don't use var here because we need to check current r headers
+                print("Has not reset, exiting.")
+                os.exit(1)
 
-        data = {
-            "object_number": jd["ObjectNumber"],
-            "category": jd["Classification"],
-            "date_description": jd["Dated"],
-            "year_start": jd["DateBegin"],
-            "year_end": jd["DateEnd"],
-            "materials": jd["Medium"],
-            "dimensions": jd["Dimensions"].replace("\r\n", ""),
-            "credit_line": jd["CreditLine"],
-            "provenance": jd["Provenance"],
-            "title": jd["Title"],
-            "technique": jd["Style"],
-            "from_location": jd["Geography"].replace("\\u", "\\u"),
-            "maker_full_name": "|".join([x["Artist"] for x in jd["Artists"]]),
-            "maker_role": "|".join([x["Role"] for x in jd["Artists"]]),
-            "current_location": jd["Location"],
-            "image_url": jd["Image"],
-            "source_1": url,
-            "drop_me": i
-        }
+        if r.status_code != 500:
+            jd = r.json()
 
-        try:
-            data["maker_birth_year"] = "|".join([x["Artist_Info"].split(",")[-1].split("-")[0].strip() for x in jd["Artists"]])
-            data["maker_death_year"] = "|".join([x["Artist_Info"].split(",")[-1].split("-")[-1].strip() for x in jd["Artists"]])
-        except AttributeError:
-            print("Artist info is null")
-            print(jd["Artists"])
+            data = {
+                "object_number": jd["ObjectNumber"],
+                "category": jd["Classification"],
+                "date_description": jd["Dated"],
+                "year_start": jd["DateBegin"],
+                "year_end": jd["DateEnd"],
+                "materials": jd["Medium"],
+                "credit_line": jd["CreditLine"],
+                "provenance": jd["Provenance"],
+                "title": jd["Title"],
+                "technique": jd["Style"],
+                "maker_full_name": "|".join([x["Artist"] for x in jd["Artists"]]),
+                "maker_role": "|".join([x["Role"] for x in jd["Artists"]]),
+                "current_location": jd["Location"],
+                "image_url": jd["Image"],
+                "source_1": url,
+                "drop_me": i
+            }
 
-        if len(jd["Artists"]) == 1:
-            if "unknown" in jd["Artists"][0]["Artist"]:
-                data["maker_role"] = ""
-                print("Nulling maker_role for", jd["Artists"])
+            if len(jd["Artists"]) > 1:
+                try:
+                    data["maker_birth_year"] = "|".join([x["Artist_Info"].split(",")[-1].split("-")[0].strip() if x["Artist_Info"] else "" for x in jd["Artists"]])
+                    data["maker_death_year"] = "|".join([x["Artist_Info"].split(",")[-1].split("-")[-1].strip() if x["Artist_Info"] else "" for x in jd["Artists"] if x["Artist_Info"]])
+                except AttributeError:
+                    print("Artist info is null")
+                    print(jd["Artists"])
 
-        writer.writerow(data)
+            elif len(jd["Artists"]) == 1:
+                if "unknown" in jd["Artists"][0]["Artist"]:
+                    data["maker_role"] = ""
+                    print("Nulling maker_role for", jd["Artists"])
+
+            if jd["Dimensions"]:
+                data["dimensions"] = jd["Dimensions"].replace("\r\n", "")
+
+            if jd["Geography"]:
+                data["from_location"] = jd["Geography"].replace("\\u", "\\u")
+
+            writer.writerow(data)
