0a46c4a35912aa8a377cd360bee5f146b460322b
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-07-02 21:55:10 -0400

    add MAA stuff

--- 9_arts/maa/scraper.py
+++ 9_arts/maa/scraper.py
@@ -22,7 +22,7 @@ with open(filename, "a", encoding='utf-8', newline='') as output_file:
     writer = csv.DictWriter(output_file, fieldnames=columns)
 
     if os.path.exists(filename) and os.stat(filename).st_size > 299:
-        next = get_last_id(filename, 299, "source_2")
+        next = get_last_id(filename, 299, "source_2", url=True)
         # start_id = 3320
     else:
         next = "https://collections.maa.cam.ac.uk/objects-api/objects"
@@ -31,7 +31,7 @@ with open(filename, "a", encoding='utf-8', newline='') as output_file:
         writer.writeheader()
 
     s = requests.Session()
-    with tqdm(total=17027) as pbar:
+    with tqdm(total=7429) as pbar:
         while next:
             r = s.get(next)
             jdr = r.json()
@@ -49,13 +49,13 @@ with open(filename, "a", encoding='utf-8', newline='') as output_file:
                             "institution_longitude": 0.1212875687220559,
                             "object_number": jd["id"],
                             "accession_number": jd["main_ref_no"]["number"],
-                            "culture": "|".join(jd["culture_group"]),
+                            "culture": "|".join([x["name"] for x in jd["culture_group"]]),
                             "date_description": "|".join([x["name"] for x in jd["period"]]),
                             "materials": "|".join([x["name"] for x in jd["material"]]),
                             "from_location": jd["de_norm_place"],
                             "credit_line": jd["source"],
                             "description": jd["description"].replace("<br />", ""),
-                            "department": jd["department"]["name"],
+                            "department": jd.get("department", {}).get("name", None) if jd["department"] else None,
                             "dimensions": jd["dimensions"],
                             "image_url": jd["images"][0] if len(jd["images"]) else None,
                             "source_1": jd["facebook_share"].split("?u=")[-1],
