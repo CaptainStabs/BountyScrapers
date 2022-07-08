e99f3e6fc357708d6c9eaf674ed82f2143ab2d1f
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-07-08 18:26:39 -0400

    update scraper

--- 9_arts/colbase/scraper.py
+++ 9_arts/colbase/scraper.py
@@ -113,7 +113,7 @@ def scraper(filename, mus_info, org_id):
                     else:
                         death = None
 
-                    names = "|".join(re.split(re.compile(r"(and|, |;)"), re.sub(by_pat, "", re.sub(names_only, "", a)))).replace("Compiled by", "").replace("illustrated by", "").replace("Illustrated by", "") .replace("illustrated by", "").replace("Illustrated by", "") if item["sakusha"] else None
+                    names = "|".join(re.split(re.compile(r"( and |, |;)"), re.sub(by_pat, "", re.sub(names_only, "", a)))).replace("Compiled by", "").replace("illustrated by", "").replace("Illustrated by", "") .replace("illustrated by", "").replace("Illustrated by", "") if item["sakusha"] else None
 
                     data = {
                         "institution_name": mus_info["institution_name"],
@@ -145,6 +145,12 @@ def scraper(filename, mus_info, org_id):
                     if mby:
                         if len(str(mby)) == 8:
                             data["maker_birth_year"] = int(str(mby)[:4])
+
+                            if not mdy:
+                                data["maker_death_year"] = int(str(mby)[4:])
+
+                        elif len(str(strmby)) == 6:
+                            data["maker_death_year"] = int(str(mby)[:4])
 
                             if not mdy:
                                 data["maker_death_year"] = int(str(mby)[4:])
