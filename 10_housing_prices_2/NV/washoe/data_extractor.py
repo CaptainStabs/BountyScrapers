f4b0d2572de9381d566bf1b445c471a9e97ca5a5
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-09-15 11:21:32 -0400

    update Washoe

--- 10_housing_prices_2/NV/washoe/data_extractor.py
+++ 10_housing_prices_2/NV/washoe/data_extractor.py
@@ -11,7 +11,7 @@ from _common import date_parse
 
 
 # ParcelID,DeedType,Grantor,Grantee,SaleDate,SaleAmount
-columns = ["property_id", "physical_address", "sale_type", "seller_name", "buyer_name", "sale_date", "sale_price", "county", "state", "source_url", "land_appraised_value", "land_assessed_value", "building_appraised_value", "building_assessed_value", "land_area_sqft", "land_area_acres", "building_area_sqft"]
+columns = ["property_id", "property_street_address", "sale_type", "seller_name", "buyer_name", "sale_datetime", "sale_price", "property_county", "state", "source_url", "land_appraised_value", "land_assessed_value", "building_appraised_value", "building_assessed_value", "land_area_sqft", "land_area_acres", "building_area_sqft", "transfer_deed_type"]
 with open("Parcels.csv", "r") as input_csv:
     line_count = len([line for line in input_csv.readlines()])
     input_csv.seek(0)
@@ -22,50 +22,50 @@ with open("Parcels.csv", "r") as input_csv:
         writer.writeheader()
 
         for row in tqdm(reader, total=line_count):
-            try:
-                land_info = {
-                    "property_id": row["ParcelID"],
-                    "seller_name": " ".join(str(row["Grantor"]).upper().split()),
-                    "buyer_name": " ".join(str(row["Grantee"]).upper().split()),
-                    "sale_date": str(date_parse(row["SaleDate"])),
-                    "sale_price": row["SaleAmount"],
-                    "county": "WASHOE",
-                    "state": "NV",
-                    "source_url": "https://gis.washoecounty.us/webservices/gisdataservice/DocumentFiles/AssessorOnlineReports/A7BE5E322AA4WC531ADMINCF8BC6597CX2B4/?fileId=GSAQuickInfo_2021Final.zip",
-                    "land_appraised_value": row["LandAppraised"],
-                    "land_assessed_value": row["LandAssessed"],
-                    "building_appraised_value": row["ImprovmentAppraised"],
-                    "building_assessed_value": row["ImprovmentAssessed"],
-                    "building_area_sqft": row["BldgSquareFeet"]
 
-                }
+            land_info = {
+                "property_id": row["ParcelID"],
+                "seller_name": " ".join(str(row["Grantor"]).upper().split()),
+                "buyer_name": " ".join(str(row["Grantee"]).upper().split()),
+                "sale_datetime": str(date_parse(row["SaleDate"])),
+                "sale_price": row["SaleAmount"],
+                "property_county": "WASHOE",
+                "state": "NV",
+                "source_url": "https://gis.washoecounty.us/webservices/gisdataservice/DocumentFiles/AssessorOnlineReports/A7BE5E322AA4WC531ADMINCF8BC6597CX2B4/?fileId=GSAQuickInfo_2021Final.zip",
+                "land_appraised_value": row["LandAppraised"],
+                # "land_assessed_value": row["LandAssessed"],
+                "building_appraised_value": row["ImprovmentAppraised"],
+                # "building_assessed_value": row["ImprovmentAssessed"],
+                "building_area_sqft": row["BldgSquareFeet"]
 
-                if row["LandUnitType"] == "SF":
-                    land_info["land_area_sqft"] = row["LandArea"]
-                elif row["LandUnitType"] == "AC":
-                    land_info["land_area_acres"] = row["LandArea"]
-                if row["DeedType"].strip():
-                    try:
-                        land_info["sale_type"] = str(deed_type[row["DeedType"]]).upper()
-                    except KeyError:
-                        # print(row["DeedType"])
-                        land_info["sale_type"] = row["DeedType"].upper().strip()
-                else:
-                    try:
-                        land_info["sale_type"] = str(deed_type[str(row["SaleVerificationCode"]).upper()]).upper()
-                    except KeyError:
-                        land_info["sale_type"] = str(row["SaleVerificationCode"]).upper()
-                 # ,,,
-                # If address is in separate fields
-                street_list = [str(row["SitusNumber"]).strip(), str(row["SitusDirection"]).strip(), str(row["SitusStreet"]).strip()]
+            }
 
-                # concat the street parts filtering out blank parts
-                land_info["physical_address"] = ' '.join(filter(None, street_list)).upper()
+            if row["LandUnitType"] == "SF":
+                land_info["land_area_sqft"] = row["LandArea"]
+            elif row["LandUnitType"] == "AC":
+                land_info["land_area_acres"] = row["LandArea"]
+            if row["DeedType"].strip():
+                try:
+                    land_info["transfer_deed_type"] = str(deed_type[row["DeedType"]]).upper()
+                except KeyError:
+                    # print(row["DeedType"])
+                    land_info["transfer_deed_type"] = row["DeedType"].upper().strip()
+            else:
+                try:
+                    land_info["transfer_deed_type"] = str(deed_type[str(row["SaleVerificationCode"]).upper()]).upper()
+                except KeyError:
+                    land_info["transfer_deed_type"] = str(row["SaleVerificationCode"]).upper()
+             # ,,,
+            # If address is in separate fields
+            street_list = [str(row["SitusNumber"]).strip(), str(row["SitusDirection"]).strip(), str(row["SitusStreet"]).strip()]
 
-                year = land_info["sale_date"].split("-")[0]
+            # concat the street parts filtering out blank parts
+            land_info["property_street_address"] = ' '.join(filter(None, street_list)).upper()
 
-                if land_info["physical_address"] and land_info["sale_date"] and land_info["sale_price"] != "" and int(year) <= 2022:
-                    writer.writerow(land_info)
+            year = land_info["sale_datetime"].split("-")[0]
 
-            except parser._parser.ParserError:
-                pass
+            if land_info["property_street_address"] and land_info["sale_datetime"] and land_info["sale_price"] != "" and int(year) <= 2022:
+                writer.writerow(land_info)
+
+            # except parser._parser.ParserError:
+            #     pass
