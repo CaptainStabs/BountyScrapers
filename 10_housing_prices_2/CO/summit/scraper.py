3aa6e2c22bd4dfe625a2c750c787406d86a253f2
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-09-30 09:51:10 -0400

    add crash message

--- 10_housing_prices_2/CO/summit/scraper.py
+++ 10_housing_prices_2/CO/summit/scraper.py
@@ -11,6 +11,7 @@ from pathlib import Path
 p = Path(__file__).resolve().parents[2]
 sys.path.insert(1, str(p))
 from _common import date_parse
+from _common.send_mail import send_mail
 
 
 sale_types = json.load(open("./files/doctypes.json", "r"))
@@ -26,31 +27,35 @@ with open("extracted_data.csv", "r", encoding="utf-8") as f:
         writer = csv.DictWriter(output_csv, fieldnames=columns)
 
         for data in tqdm(reader, total=line_count):
-            url = f"http://gis.summitcountyco.gov/Map/DetailData.aspx?Schno={data['property_id']}"
-            data["source_url"] = url
-            r = requests.get(url).text
+            try:
+                url = f"http://gis.summitcountyco.gov/Map/DetailData.aspx?Schno={data['property_id']}"
+                data["source_url"] = url
+                r = requests.get(url).text
 
-            soup = BeautifulSoup(r, "html.parser")
+                soup = BeautifulSoup(r, "html.parser")
 
-            rows = soup.find("table").find_all("tr")
+                rows = soup.find("table").find_all("tr")
 
-            # cols = ["id", "date", "type", "price"]
-            for row in rows:
-                sale_row = row.find_all("td", class_="style2b") # style2b is only assigned to the sale-related cells
-                if sale_row: # Filter out blank rows
-                    sale_row = [x.get_text() for x in sale_row] # Get the text from each item in the sale_row list
+                # cols = ["id", "date", "type", "price"]
+                for row in rows:
+                    sale_row = row.find_all("td", class_="style2b") # style2b is only assigned to the sale-related cells
+                    if sale_row: # Filter out blank rows
+                        sale_row = [x.get_text() for x in sale_row] # Get the text from each item in the sale_row list
 
-                    if len(sale_row) != 4:
-                        print("Sale row is not 4\n url") # add url once we get there
+                        if len(sale_row) != 4:
+                            print("Sale row is not 4\n url") # add url once we get there
 
-                    sale_data = dict(zip(cols, sale_row))
-                    data["sale_id"] = sale_data["id"]
-                    try:
-                        data["transfer_deed_type"] = sale_types[sale_data["type"]]
-                    except KeyError:
-                        # print(f"Type keyerror '{json.dumps(sale_data)}'")
-                        pass
-                    data["sale_datetime"] = date_parse(sale_data["date"])
-                    data["sale_price"] = sale_data["price"]
+                        sale_data = dict(zip(cols, sale_row))
+                        data["sale_id"] = sale_data["id"]
+                        try:
+                            data["transfer_deed_type"] = sale_types[sale_data["type"]]
+                        except KeyError:
+                            # print(f"Type keyerror '{json.dumps(sale_data)}'")
+                            pass
+                        data["sale_datetime"] = date_parse(sale_data["date"])
+                        data["sale_price"] = sale_data["price"]
 
-                    writer.writerow(data)
+                        writer.writerow(data)
+            except:
+                tb.print_exc()
+                send_mail("Scraper crashed")
