0b0990e440ec35c84f353419edf540b53f46bba5
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-11-02 13:42:31 -0400

    update url checker

--- 11_hospitals/ahd/url_checker.py
+++ 11_hospitals/ahd/url_checker.py
@@ -6,18 +6,37 @@ tqdm = tqdm.pandas()
 
 
 s = requests.Session()
+s.headers.update({
+    "sec-ch-ua": '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "DNT": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-User": "?1",
+    "Sec-Fetch-Dest": "document",
+})
 def check_url(url, s):
-    r = s.head(url)
+    try:
+        r = s.head(url)
+    except Exception as e:
+        raise(e)
+        return url
+
     if r.status_code == 200:
-        return True
+        return url
     elif r.status_code == 404:
         return False
 
     elif r.status_code == 301 or r.status_code == 302:
-        print(r.headers)
+        return r.headers.get("location")
     elif r.status_code != 200 and r.status_code != 404:
         print(f"\nUnknown code: {r.status_code}")
-        return True
+        print(url)
+        return url
 
 filename = "url_added.csv"
 df = pd.read_csv(filename)
