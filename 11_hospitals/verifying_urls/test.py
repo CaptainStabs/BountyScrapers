2738a1c6fd93f7369cff8466cdb45227d66d5e97
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-11-29 12:48:35 -0500

    add url verification stuff

---
+++ 11_hospitals/verifying_urls/test.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+
+def test(df):
+    checked_urls = []
+    for x in df:
+        if x == "cdm3" or x == "hpg1":
+            x = pd.NA
+        checked_urls.append(x)
+    return checked_urls
+
+
+df = pd.read_csv("non-null copy.csv")
+cdu, ciu, hpg = "cdm_url", "cdm_indirect_url", "homepage"
+df[[cdu, hpg, ciu]] = df[[cdu, hpg, ciu]].apply(test)
+print(df)
+
