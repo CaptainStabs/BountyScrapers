6105b7a80bd81aa8d7934459d01cefaecbbcad70
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2023-01-04 10:25:18 -0500

    add stuff

--- 12_quest_tic/cli_multi.py
+++ 12_quest_tic/cli_multi.py
@@ -1,23 +1,33 @@
 import multiprocessing
 import pandas as pd
-from mrfutils import import_csv_to_set, json_mrf_to_csv
+from mrfutils import json_mrf_to_csv
+from _mrf_stuff.helpers import import_csv_to_set
 from multiprocessing import Pool, Manager
 import sys
 import traceback as tb
 import argparse
 from tqdm import tqdm
 from _utils import istarmap
+from _utils import send_mail
+
+import os
 
 
 # define the master function that will be applied in parallel
 def work(url, out):
     try:
+        id = multiprocessing.current_process()._identity[0]
+        out = out + str(id) + "\\"
+
+        if not os.path.exists(out):
+            os.makedirs(out)
          # call the json_mrf_to_csv function with the given arguments
         json_mrf_to_csv(url=str(url), npi_filter=import_csv_to_set("./quest/npis.csv"), code_filter= import_csv_to_set("./quest/codes.csv"), out_dir=out)
      
     except:
         print(url, "\n", id)
         tb.print_exc()  # print the traceback for the exception
+        pass
 
 
 # define the apply_parallel function that applies the master function in parallel
@@ -30,6 +40,7 @@ def apply_parallel(df, func, out):
             for _ in tqdm(pool.istarmap(func, [(row["url"], out) for _, row in df.iterrows()]), desc="Main Process", position=0, total=len(df)):
                 pass
             pool.close()
+            send_mail("Finished", "Finished")
 
     # catch sigint to prevent unstoppable crash
     except KeyboardInterrupt:
@@ -60,6 +71,9 @@ if __name__ == '__main__':
         # sort the dataframe by the 'size' column
         if "size" in df.columns:
             df = df.sort_values(by=["size"])
+
+        df = df.iloc[500:]
+        df = df.head(500)
         # apply the work function in parallel using apply_parallel
         results = apply_parallel(df, work, args.out)
     except KeyboardInterrupt:
