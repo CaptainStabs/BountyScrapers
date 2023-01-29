26a4d5f2f08d6dbc9fea21288785027edb7aaf5f
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2023-01-28 19:26:06 -0500

    Update import_folders_push.bat

--- 12_quest_tic/tools/import tools/import_folders_push.bat
+++ 12_quest_tic/tools/import tools/import_folders_push.bat
@@ -4,17 +4,19 @@ setlocal enabledelayedexpansion
 set out_dir=%1
 set com_mess=%2
 set branch=%3
+set num_folders=%4
 
 dolt checkout main
 dolt checkout -b %3
+dolt checkout %3
 
 set tables=file insurer code price_metadata rate npi_rate
 
 
-for /L %%i in (1, 1, 10) do (
+for /L %%i in (1, 1, %4) do (
   for %%t in (%tables%) do (
     echo WRITING TABLE %%t
-    dolt table import -u %%t %out_dir%/%%i/%%t.csv --continue --quiet
+    dolt table import -u %%t %out_dir%/%%i/%%t.csv
   )
   dolt gc --shallow
 )
