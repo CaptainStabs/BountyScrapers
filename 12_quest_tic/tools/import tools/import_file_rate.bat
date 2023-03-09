b30412592b7f8e3ee0c4be05d32f526884a8461f
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2023-03-09 15:57:50 -0500

    Create import_file_rate.bat

---
+++ 12_quest_tic/tools/import tools/import_file_rate.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set out_dir=%1
+set com_mess=%2
+set branch=%3
+set num_folders=%4
+
+dolt checkout main
+dolt checkout -b %3
+dolt checkout %3
+
+set tables=file_rate
+
+
+for /L %%i in (1, 1, %4) do (
+  for %%t in (%tables%) do (
+    echo WRITING TABLE %%t
+    dolt table import -u %%t %out_dir%/%%i/%%t.csv 
+  )
+)
+
+dolt add .
+dolt commit -m "%2"
+dolt push origin %3
