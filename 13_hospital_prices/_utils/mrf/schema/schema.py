678c94947c89924f11efc56ea8cdabfbb79b1c23
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2023-03-09 15:27:52 -0500

    get stuff started

---
+++ 13_hospital_prices/_utils/mrf/schema/schema.py
@@ -0,0 +1,49 @@
+SCHEMA = {
+    "file": [
+        "id",
+        "filename",
+        "last_updated_on",
+        "url",
+    ],
+    "insurer": [
+        "id",
+        "reporting_entity_name",
+        "reporting_entity_type",
+    ],
+    "code": [
+        "id",
+        "billing_code_type_version",
+        "billing_code",
+        "billing_code_type",
+    ],
+    "price_metadata": [
+        "id",
+        "billing_class",
+        "negotiated_type",
+        "service_code",
+        "expiration_date",
+        "additional_information",
+        "billing_code_modifier",
+    ],
+    "rate": [
+        "id",
+        "insurer_id",
+        "code_id",
+        "price_metadata_id",
+        "negotiated_rate",
+    ],
+    "tin": [
+        "id",
+        "tin_type",
+        "tin_value",
+    ],
+    "tin_rate_file": [
+        "tin_id",
+        "rate_id",
+        "file_id",
+    ],
+    "npi_tin": [
+        "npi",
+        "tin_id",
+    ]
+}
---
+++ 13_hospital_prices/_utils/send_mail.py
@@ -0,0 +1,13 @@
+import smtplib
+from ._secrets import gmail_user, gmail_password, sent_from, to
+
+def send_mail(subject, message=None):
+    try:
+        smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        smtp_server.ehlo()
+        smtp_server.login(gmail_user, gmail_password)
+        smtp_server.sendmail(sent_from, to, message)
+        smtp_server.close()
+        print ("Email sent successfully!")
+    except Exception as ex:
+        print ("Something went wrong….",ex)
