91952af5fdfbe03006a26eaade24b55be2ee73f1
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2023-03-10 12:57:41 -0500

    unneeded scripts moved to obsole

--- 13_hospital_prices/anthem.py
+++
@@ -1,6 +0,0 @@
-from mrfutils import index_file_to_csv
-
-url = "https://antm-pt-prod-dataz-nogbd-nophi-us-east1.s3.amazonaws.com/anthem/2023-03-01_anthem_index.json.gz"
-out = ".\\anthem\\"
-
-index_file_to_csv(url=url, out_dir=out)
