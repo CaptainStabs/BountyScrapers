a9a66f3d124f05b07b67d8f387ad4cae4a83617e
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2023-04-06 10:46:07 -0400

    Update data_extractor.py

--- 14_transparency_pricing/beaumont/data_extractor.py
+++ 14_transparency_pricing/beaumont/data_extractor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import datetime
+from tqdm import tqdm
 
 def payer_category(payer_category):
     cats = {
@@ -17,58 +18,62 @@ def payer_category(payer_category):
 
 
 ccn_table = {
-    "752379007": "100131",
-    "BAY AREA": "520113",
-    "BURLINGTON": "520059",
-    "GRAFTON": "520207",
-    "KENOSHA": "520189",
-    "LAKELAND": "520102",
-    "MANITOWOC": "520034",
-    "OSHKOSH": "520198"
+    "dearborn": "230020",
+    "farmington": "230020",
+    "grosse": "230089",
+    "royal": "230130",
+    "taylor": "230270",
+    "trenton": "230176",
+    "troy": "230269",
+    "wayne": "230142"
 
 }
 in_dir = ".\\input_files\\"
-# for file in os.listdir(in_dir):
-    # df = pd.read_excel(in_dir + file, dtype=object, header=1)
-file = "2_381405141_beaumont-hospital-dearborn-hospital_standardcharges.csv"
-df = pd.read_csv(file, encoding="iso-8859-1")
-# df["hospital_ccn"] = ccn_table[file.split("_")[0]]
+for file in tqdm(os.listdir(in_dir)):
+    print(file)
+    df = pd.read_csv(in_dir + file, na_values='#N/A', encoding="iso-8859-1", dtype={"Code": str}, low_memory=False)
+    df = df.rename(columns={
+        " Code Type": "code_meta",
+        "Procedure ": "internal_code",
+        "Code": "code",
+        "Rev Code ": "rev_code",
+        "Rev Code": "rev_code",
+        "Procedure Description": "procedure_desc",
+        })
 
-df = df.rename(columns={
-    " Code Type": "code_meta",
-    "Procedure ": "internal_code",
-    "Code": "code",
-    "Rev Code ": "rev_code",
-    "Procedure Description": "procedure_desc",
-    })
+    # Get the columns after `procedure_desc` (which are all payers)
+    cols = df.columns.tolist()
+    payers = cols[cols.index("procedure_desc")+1:]
+    id_vars = cols[:cols.index("procedure_desc")+1]
 
+    df_new = pd.melt(df, id_vars=id_vars, value_vars=payers)
 
-# Get the columns after `procedure_desc` (which are all payers)
-cols = df.columns.tolist()
-payers = cols[cols.index("procedure_desc")+1:]
-id_vars = cols[:cols.index("procedure_desc")+1]
+    df = df_new.rename(columns={
+        "variable": "payer_desc",
+        "value": "rate"
+        }
+    )
 
-df_new = pd.melt(df, id_vars=id_vars, value_vars=payers)
-
-df = df_new.rename(columns={
-    "variable": "payer_desc",
-     "value": "rate"
-    }
-)
-
-df["code_type"] = df["code"].apply(lambda x: "ms-drg" if "MSDRG" in x else pd.NA)
-df["ms_drg"] = df["code"].apply(lambda x: x.replace("MSDRG ", "") if "MSDRG" in x else pd.NA)
+    df["code_type"] = df["code"].apply(lambda x: "ms-drg" if pd.notna(x) and "MSDRG" in x else pd.NA)
+    df["ms_drg"] = df["code"].apply(lambda x: x.replace("MSDRG ", "") if pd.notna(x) and "MSDRG" in x else pd.NA)
 
 
-df["file_last_updated"] = "2023-01-01"
-df["filename"] = file
+    df["file_last_updated"] = "2023-01-01"
+    df["filename"] = file
+    df["url"] = "https://www.beaumont.org/docs/default-source/default-document-library/cdm-documents/2023/" + file
+    df["hospital_ein"] = file.split("_")[0]
 
-df_obj = df.select_dtypes(['object'])
-df[df_obj.columns] = df_obj.apply(lambda x: x.str.strip())
+    df_obj = df.select_dtypes(['object'])
+    df[df_obj.columns] = df_obj.apply(lambda x: x.str.strip())
 
-df["code_meta"] = df["code_meta"].apply(str.lower)
+    df["rate"] = df["rate"].apply(lambda x: x.replace("$", "").strip() if (pd.notna(x) and "$" in x) else pd.NA)
 
-df["payer_category"] = df["payer_desc"].apply(lambda x: payer_category(x))
+    df["code_meta"] = df["code_meta"].apply(str.lower)
 
-print(df)
-# df.to_csv("test.csv", index=False)
+    df["payer_category"] = df["payer_desc"].apply(lambda x: payer_category(x))
+
+    df["rev_code"] = df["rev_code"].fillna("nan")
+
+    df["hospital_ccn"] = ccn_table[file.split("-")[2]]
+
+    df.to_csv(".\\output_files\\" + file.split("-")[2] + ".csv", index=False)
