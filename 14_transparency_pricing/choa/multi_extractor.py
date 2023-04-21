b6d551af1544e93946fecb1e343cd3b87abb5aaf
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2023-04-21 09:48:53 -0400

    Update multi_extractor.py

--- 14_transparency_pricing/choa/multi_extractor.py
+++ 14_transparency_pricing/choa/multi_extractor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+from tqdm import tqdm
 
 
 def patient_class(x):
@@ -34,12 +35,16 @@ def code_prefix(x):
         'Custom': 'custom'
     }
 
-    if " " in x:
+    if len(str(x.split()[-1])) == 8:
+        return 'custom'
+    
+    elif " " in x:
         prefix = x.split(" ")[0]
         try:
             return prefixes[prefix]
         except KeyError:
-            print(x)
+            # print(x)
+            pass
     else:
         return 'none'
 
@@ -52,9 +57,7 @@ def zero_pad(code, code_type):
 
 folder = ".\\input_files\\"
 
-file = '58-0572465_Childrens_Healthcare_of_ATL_at_Scottish_Rite_standardcharges.xlsx'
-ein = file.split("_")[0]
-
+# file = '58-0572465_Childrens_Healthcare_of_ATL_at_Scottish_Rite_standardcharges.xlsx'
 ccn = {
     '58-0572465': '113301',
     '58-0572412': '113300',
@@ -62,9 +65,15 @@ ccn = {
 }
 
 
-for file in os.listdir(folder):
-        
-    df = pd.read_excel(folder + file, skiprows=1, dtype={'Code': str, 'Procedure': str, 'NDC': str, 'Rev Code': str})
+for file in tqdm(os.listdir(folder)):
+    print(file)
+    ein = file.split("_")[0]
+    if '.csv' in file:
+        df = pd.read_csv(folder + file, dtype={'Code': str, 'Procedure': str, 'NDC': str, 'Rev Code': str})
+        file = file.replace('.csv', '.xlsx')
+    else:
+        df = pd.read_excel(folder + file, skiprows=1, dtype={'Code': str, 'Procedure': str, 'NDC': str, 'Rev Code': str})
+    
 
 
     df.rename(columns={
@@ -74,6 +83,7 @@ for file in os.listdir(folder):
         'NDC': 'ndc',
         'Rev Code': 'rev_desc',
         'Procedure Description': 'description',
+        'Quantity': 'ndc_quantity_desc'
     }, inplace=True)
 
 
@@ -91,27 +101,44 @@ for file in os.listdir(folder):
     df['rev_code'] = df['rev_desc'].apply(lambda x: str(x).split(' - ')[0])
 
     df['code_type'] = df['code_type'].str.replace('Champus DRG - v35', 'drg')
+    df['code_type'] = df['code_type'].str.replace('champus drg - v33', 'drg')
 
     zero_pad_func = np.vectorize(zero_pad)
     df['code'] = zero_pad_func(df['code_orig'], df['code_type'])
     df['code_prefix'] = df['code'].apply(code_prefix)
 
     df['code'] = df['code'].apply(lambda x: str(x).split()[-1] if ' ' in str(x) else x)
+    
 
 
     df['internal_code'].fillna('na', inplace=True)
     df['code'].fillna('na', inplace=True)
     df['rev_code'].fillna('na', inplace=True)
+    df['rev_code'] = df['rev_code'].str.replace('nan', 'na')
+    df['ndc'].fillna('na', inplace=True)
+    df['code_prefix'].fillna('none', inplace=True)
+
+    is_hcpcs_cpt_and_length_6 = df['code_prefix'] == 'hcpcs_cpt'
+    is_hcpcs_cpt_and_length_6 &= df['code'].str.len() != 6
+    df.loc[~is_hcpcs_cpt_and_length_6, 'code_prefix'] = 'custom'
 
 
     df['payer_name'] = df['payer_orig']
     df['code_type'] = df['code_type'].str.lower()
 
+    df['code_type'] = df['code_type'].str.replace('champus drg - v33', 'drg') # Not sure why this has to be down here
+    df.loc[df['code_type'].isin(['ed', 'ed ', 'er', 'ct', 'mri', 'ip per diem']), 'code_type'] = None
     df['filename'] = file
     df['hospital_ein'] = ein
     df['hospital_ccn'] = ccn[ein]
     df['file_last_updated'] = '2023-01-01'
+    df['url'] = 'https://choaassets.choa.org/images/files/' + file
 
-    df.to_csv(ein + '.csv', index=False)
+    df_obj = df.select_dtypes(['object'])
+    df[df_obj.columns] = df_obj.apply(lambda x: x.str.strip())
+    
+    df.dropna(subset=['rate'], inplace=True)
+
+    df.to_csv('.\\output_files\\' + ein + '.csv', index=False)
 
 
