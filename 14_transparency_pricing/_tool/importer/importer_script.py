dfa5e808892678b9ecf1cdedf482e36c901768cb
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2023-06-21 12:34:59 -0400

    Update importer_script.py

--- 14_transparency_pricing/_tool/importer/importer_script.py
+++ 14_transparency_pricing/_tool/importer/importer_script.py
@@ -2,17 +2,26 @@ import os
 from tqdm import tqdm
 import argparse
 
-def importer():
+def importer(hospital):
     folder = os.getcwd()
+
+    if hospital:
+        file = os.path.join(folder, hospital)
+        os.system(f'cd /D F:\\_Bounty\\transparency-in-pricing\\ & dolt table import -u hospital "{file}"')
+
     folder = os.path.join(folder, 'output_files')
 
     for file in os.listdir(folder):
-        print(file)
+        print('\n', file)
         path = os.path.join(folder, file)
         os.system(f'cd /D F:\\_Bounty\\transparency-in-pricing\\ & dolt table import -u rate "{path}"')
 
 def cli():
-    importer()
+    parser = argparse.ArgumentParser(description="Iteratively import data from child folder 'input_files`. Optionally include name of hospital csv to import that as well.")
+    parser.add_argument('hospital', type=str, nargs='?', default=False, help="Name of hospital csv to import")
+    # parser.add_argument('-f', '--hospital', type=str, required=False, help="Name of hospital csv to import")
+    args = parser.parse_args()
+    importer(args.hospital)
 
 if __name__ == "__main__":
     cli()
